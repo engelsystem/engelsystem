aeb149b7a3aa8de0e416f39c627ce68b8215503e
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2005-11-11 22:58:35 +0000

    interfeve fertig gestellt
    git-svn-id: svn://svn.cccv.de/engel-system@48 29ba0400-6e00-0410-a75a-ca02368028f8

--- www-ssl/admin/userDefaultSetting.php
+++ www-ssl/admin/userDefaultSetting.php
@@ -1,52 +1,52 @@
 <?PHP
 
-$title = "Debug-Liste";
-$header = "Datenbank-Auszug";
+$title = "Defalut User Setting";
+$header = "Defalut User Setting";
 include ("./inc/header.php");
 include ("./inc/funktion_db_list.php");
 
+echo "Hallo ".$_SESSION['Nick'].
+	",<br>\nhier hast du die M&ouml;glichkeit, die Defaulteinstellungen f&uuml;r neue User einzustellen:<br><br>\n";
+				
 
-/*
-echo "<h1>Raeume</h1> <br>";
-funktion_db_list("Raeume");
-
-echo "<h1>Schichtbelegung</h1> <br>";
-funktion_db_list("Schichtbelegung");
-
-echo "<h1>Schichtplan</h1> <br>Hier findest du alle bisher eingetragenen Schichten:";
-funktion_db_list("Schichtplan");
-
-echo "<h1>User</h1> <br>";
-funktion_db_list("User");
-
-echo "<h1>News</h1> <br>";
-funktion_db_list("News");
-
-echo "<h1>FAQ</h1> <br>";
-funktion_db_list("FAQ");
-
-*/
-
-echo "Deaktiviert<br>\n";
+if( isset( $_GET["Field"]) && isset( $_GET["Default"]))
+{
+	$SQL = "ALTER TABLE `UserCVS` CHANGE `". $_GET["Field"]. "` ".
+		"`". $_GET["Field"]. "` CHAR( 1 ) NOT NULL DEFAULT '". $_GET["Default"]. "'";
+	$erg = mysql_query( $SQL, $con);
+	if( $erg == 1)
+		echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " succesfull</h2>\n";
+	else
+		echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " error...</h2>\n".
+			"[". mysql_error(). "]<br><br>";
+}
 
 $erg = mysql_query("SHOW COLUMNS FROM `UserCVS`");
 echo mysql_error();
 
-if (mysql_num_rows($erg) > 0) 
-{
-/*	while ($row = mysql_fetch_assoc($erg))
-	{
-		print_r($erg);
-	}
-*/
-}
+echo "<table border=\"0\" class=\"border\">\n";
+echo "\t<tr class=\"contenttopic\">\n";
+echo "\t\t<th>Page</th>\n\t\t<th>Show</th>\n\t\t<th></th>\n";
+echo "\t</tr>\n";
 for( $i=1; $i<mysql_num_rows($erg); $i++)
 {
-	echo "";
-	echo  mysql_result( $erg, $i, "Field");
-	echo  mysql_result( $erg, $i, "Default");
-	echo "<br>";
+	echo "\t<tr class=\"content\">\n";
+	echo "\t\t<form action=\"userDefaultSetting.php\">\n";
+	echo "\t\t\t<input name=\"Field\" type=\"hidden\" value=\"". mysql_result( $erg, $i, "Field"). "\">\n";
+	echo "\t\t\t<td>". mysql_result( $erg, $i, "Field"). "</td>\n";
+	echo "\t\t\t<td>";
+	if( mysql_result( $erg, $i, "Default") == "Y")
+		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\" checked>Y\t".
+			"\t\t\t\t<input type=\"radio\" name=\"Default\" value=\"N\">N";
+	else
+		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\">Y\t".
+			"\t\t\t\t<input type=\"radio\" name=\"Default\" value=\"N\" checked>N";
+	echo "</td>\n";
+	echo "\t\t\t<td><input type=\"submit\" value=\"Save\"></td>\n";
+	echo "\t\t</form>\n";
+	echo "\t</tr>\n";
 }
+echo "</table>\n";
 
 include ("./inc/footer.php");
 ?>
