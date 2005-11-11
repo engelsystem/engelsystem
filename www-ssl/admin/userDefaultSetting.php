71fffc65fd438adb36eca677fa5a5ad9fa242a1f
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2005-11-11 23:51:00 +0000

    add function: new entry & del entry
    git-svn-id: svn://svn.cccv.de/engel-system@49 29ba0400-6e00-0410-a75a-ca02368028f8

--- www-ssl/admin/userDefaultSetting.php
+++ www-ssl/admin/userDefaultSetting.php
@@ -9,17 +9,41 @@ echo "Hallo ".$_SESSION['Nick'].
 	",<br>\nhier hast du die M&ouml;glichkeit, die Defaulteinstellungen f&uuml;r neue User einzustellen:<br><br>\n";
 				
 
-if( isset( $_GET["Field"]) && isset( $_GET["Default"]))
+if( isset( $_GET["Field"]) && isset( $_GET["Default"]) && isset( $_GET["Send"]))
 {
-	$SQL = "ALTER TABLE `UserCVS` CHANGE `". $_GET["Field"]. "` ".
-		"`". $_GET["Field"]. "` CHAR( 1 ) NOT NULL DEFAULT '". $_GET["Default"]. "'";
-	$erg = mysql_query( $SQL, $con);
-	if( $erg == 1)
-		echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " succesfull</h2>\n";
-	else
-		echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " error...</h2>\n".
-			"[". mysql_error(). "]<br><br>";
-}
+	switch( $_GET["Send"])
+	{
+		case "New":
+ 			$SQL = "ALTER TABLE `UserCVS` ADD `". $_GET["Field"]. "` ".
+				"CHAR( 1 ) DEFAULT '". $_GET["Default"]. "' NOT NULL";
+			$erg = mysql_query( $SQL, $con);
+			if( $erg == 1)
+				echo "<H2>Create ".$_GET["Field"]. " = ". $_GET["Default"]. " succesfull</h2>\n";
+			else
+				echo "<H2>Create ".$_GET["Field"]. " = ". $_GET["Default"]. " error...</h2>\n".
+					"[". mysql_error(). "]<br><br>";
+			break;
+		case "Del":
+			$SQL = "ALTER TABLE `UserCVS` DROP `". $_GET["Field"]. "` ";
+			$erg = mysql_query( $SQL, $con);
+			if( $erg == 1)
+				echo "<H2>Delete ".$_GET["Field"]. " succesfull</h2>\n";
+			else
+				echo "<H2>Delete ".$_GET["Field"]. " error...</h2>\n".
+					"[". mysql_error(). "]<br><br>";
+			break;
+		case "Save":
+			$SQL = "ALTER TABLE `UserCVS` CHANGE `". $_GET["Field"]. "` ".
+				"`". $_GET["Field"]. "` CHAR( 1 ) NOT NULL DEFAULT '". $_GET["Default"]. "'";
+			$erg = mysql_query( $SQL, $con);
+			if( $erg == 1)
+				echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " succesfull</h2>\n";
+			else
+				echo "<H2>Write ".$_GET["Field"]. " = ". $_GET["Default"]. " error...</h2>\n".
+					"[". mysql_error(). "]<br><br>";
+			break;
+	} //SWITCH
+} //IF(
 
 $erg = mysql_query("SHOW COLUMNS FROM `UserCVS`");
 echo mysql_error();
@@ -36,16 +60,30 @@ for( $i=1; $i<mysql_num_rows($erg); $i++)
 	echo "\t\t\t<td>". mysql_result( $erg, $i, "Field"). "</td>\n";
 	echo "\t\t\t<td>";
 	if( mysql_result( $erg, $i, "Default") == "Y")
-		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\" checked>Y\t".
-			"\t\t\t\t<input type=\"radio\" name=\"Default\" value=\"N\">N";
+		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\" checked>Y\n".
+			"\t\t\t    <input type=\"radio\" name=\"Default\" value=\"N\">N";
 	else
-		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\">Y\t".
-			"\t\t\t\t<input type=\"radio\" name=\"Default\" value=\"N\" checked>N";
+		echo	"<input type=\"radio\" name=\"Default\" value=\"Y\">Y\n".
+			"\t\t\t    <input type=\"radio\" name=\"Default\" value=\"N\" checked>N";
 	echo "</td>\n";
-	echo "\t\t\t<td><input type=\"submit\" value=\"Save\"></td>\n";
+	echo "\t\t\t<td><input type=\"submit\" name=\"Send\" value=\"Save\">\n";
+	echo "\t\t\t    <input type=\"submit\" name=\"Send\" value=\"Del\"></td>\n";
 	echo "\t\t</form>\n";
 	echo "\t</tr>\n";
 }
+	
+echo "\t<tr class=\"content\">\n";
+echo "\t\t<form action=\"userDefaultSetting.php\">\n";
+echo "\t\t\t<input name=\"New\" type=\"hidden\" value=\"New\">\n";
+echo "\t\t\t<td><input name=\"Field\" type=\"text\" value=\"new\"></td>\n";
+echo "\t\t\t<td><input type=\"radio\" name=\"Default\" value=\"Y\">Y\t".
+	"\t\t\t\t<input type=\"radio\" name=\"Default\" value=\"N\">N</td>\n";
+echo "\t\t\t<td><input type=\"submit\" name=\"Send\" value=\"New\"></td>\n";
+echo "\t\t</form>\n";
+echo "\t</tr>\n";
+
+
+
 echo "</table>\n";
 
 include ("./inc/footer.php");
