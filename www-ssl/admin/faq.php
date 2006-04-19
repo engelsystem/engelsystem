e26c8ad1ac80bc5a849ca56d1a01d9cff6d2b18a
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2006-04-19 21:01:04 +0000

    typo, aufgereumt
    git-svn-id: svn://svn.cccv.de/engel-system@127 29ba0400-6e00-0410-a75a-ca02368028f8

--- www-ssl/admin/faq.php
+++ www-ssl/admin/faq.php
@@ -8,7 +8,8 @@ include ("./inc/funktion_user.php");
 //var init
 $quest_bearb=0;
 
-if (IsSet($_GET["quest"])) {
+if (IsSet($_GET["quest"])) 
+{
 
 switch ($_GET["quest"]) 
 {
@@ -21,7 +22,7 @@ switch ($_GET["quest"])
 // *---------------------------------------------------------------------------
 
 case "all":
-	$SQL="Select * from Questions ORDER BY QID DESC";
+	$SQL="SELECT * FROM `Questions` ORDER BY QID DESC";
 ?>
 	Alle Anfragen:<br>
         <table  width="100%" class="border" cellpadding="2" cellspacing="1">
@@ -44,116 +45,94 @@ case "all":
 		echo "<td>".mysql_result($Erg, $n, "Question")."</td>\n";
 		echo "<td>".UID2Nick(mysql_result($Erg, $n, "UID"))."</td>\n";
 		echo "<td>";
-		if (mysql_result($Erg, $n, "AID")>0) { 
+		if (mysql_result($Erg, $n, "AID")>0) 
+		{ 
 			echo "Ja</td>\n";
 			echo "<td>".mysql_result($Erg, $n, "Answer")."</td>\n";
 			echo "<td>".UID2Nick(mysql_result($Erg, $n, "AID"))."</td>\n";
-		} else {
+		} 
+		else 
+		{
 			echo "Nein</td>\n";
 			echo "<td>&nbsp;</td>\n";
 			echo "<td>&nbsp;</td>\n";
 		}
-				echo "<td><a href=\"faq.php?quest=edit&QID=".mysql_result($Erg, $n, "QID")."\">xxx</a></td>";
+		echo "<td><a href=\"faq.php?quest=edit&QID=".mysql_result($Erg, $n, "QID")."\">xxx</a></td>";
 		echo "</tr>\n";
 	}
 	echo "</table>\n";
 	break;
+	
 case "open":
-	$SQL="Select * from Questions where AID = \"0\" ORDER BY QID DESC";
+	$SQL="SELECT * FROM `Questions` WHERE AID = \"0\" ORDER BY QID DESC";
 	$quest_bearb=1; // Fragenliste anzeigen
-?>
-		Offene Anfragen:<br>
-<?php
+	echo "\t\tOffene Anfragen:<br>\n";
 	break;
+	
 case "edit":
 	$quest_bearb=0; // keine Fragenliste anzeigen, Frage editieren...
-	if (!IsSet($_GET["QID"])){
-?>
-		Fehlerhafter Aufruf...<br>Bitte die Bearbeitung nochmals beginnen :)
-<?php
-	} else {
-	$SQL = "SELECT * FROM Questions where QID=". $_GET["QID"];
-	$Erg = mysql_query($SQL, $con);
-?>
-	<form action="./faq.php" method="GET">
-		Anfrage von <b><?php echo UID2NICK(mysql_result($Erg, 0, "UID")); ?></b>:<br>
-		<textarea name="Question" rows="3" cols="80"><?php echo mysql_result($Erg, 0, "Question"); ?></textarea>
-		<br><br>
-		Antwort der Erzengel:<br>
-<?php
-	if (mysql_result($Erg, 0, "Answer")=="") {
-?>
-		<textarea name="Answer" rows="5" cols="80">Bitte hier die Antwort eintragen...</textarea>
-		<br>
-<?php 
-	} else {
-?>
-		<textarea name="Answer" rows="5" cols="80"><?php echo mysql_result($Erg, 0, "Answer"); ?></textarea>
-		<br>
-<?php
-	}
-?>
-		<input type="hidden" name="QID" value="<?PHP echo $_GET["QID"]; ?>">
-		<input type="hidden" name="quest" value="save">
-		<input type="submit" value="Sichern...">
-	</form>
-	Wenn diese Anfrage bereits beantwortet wurde, kannst du diese so wie sie ist als Engel-FAQ eintrag &uuml;bernehmen.<br>
-	In diesem Falle erscheint hier der Link: 
-<?php
-	if (mysql_result($Erg, 0, "AID")<>"0") {
-?>
-	<a href="./faq.php?quest=transfer&QID=<?php echo $QID; ?>">Als FAQ-Eintrag sichern...</a>
-<?php
-	}
-	
+	if (!IsSet($_GET["QID"]))
+		echo "\t\tFehlerhafter Aufruf...<br>Bitte die Bearbeitung nochmals beginnen :)\n";
+	else 
+	{
+		$SQL = "SELECT * FROM Questions where QID=". $_GET["QID"];
+		$Erg = mysql_query($SQL, $con);
+		echo "\t\t<form action=\"./faq.php\" method=\"GET\">\n";
+		echo "\t\tAnfrage von <b>". UID2NICK(mysql_result($Erg, 0, "UID")). "</b>:<br>\n";
+		echo "\t\t<textarea name=\"Question\" rows=\"3\" cols=\"80\">".
+			mysql_result($Erg, 0, "Question"). "</textarea>\n";
+		echo "<br><br>Antwort der Erzengel:<br>\n";
+		if (mysql_result($Erg, 0, "Answer")=="") 
+			echo "\t\t<textarea name=\"Answer\" rows=\"5\" cols=\"80\">".
+				"Bitte hier die Antwort eintragen...</textarea><br>\n";
+		else 
+			echo "\t\t<textarea name=\"Answer\" rows=\"5\" cols=\"80\">". 
+				mysql_result($Erg, 0, "Answer"). "</textarea>\n<br>\n";
+		echo "\t\t<input type=\"hidden\" name=\"QID\" value=\"". $_GET["QID"]. "\">\n";
+		echo "\t\t<input type=\"hidden\" name=\"quest\" value=\"save\">\n";
+		echo "\t\t<input type=\"submit\" value=\"Sichern...\">\n";
+		echo "\t</form>\n";
+		if (mysql_result($Erg, 0, "AID")<>"0") 
+		{
+			echo "\tDu kannst diese Anfrage so wie sie ist, als Engel-FAQ eintrag &uuml;bernehmen.<br>\n";
+			echo "<a href=\"./faq.php?quest=transfer&QID=". $_GET["QID"]. "\">Als FAQ-Eintrag sichern...</a>\n";
+		}
 	} // Abfrage der QID
 	break;
 
 case "save":
-	if (!IsSet($_GET["QID"])){
-?>
-	  Fehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...
-<?php
-        } else {
-	  $SQL = "UPDATE Questions SET Question=\"". $_GET["Question"]. 
-	  	 "\", AID=\"". $_SESSION['UID']. "\" , Answer=\"". $_GET["Answer"]. "\" ".
-		 "where QID = \"". $_GET["QID"]. "\" LIMIT 1";
-          $Erg = mysql_query($SQL, $con);
-          if ($Erg == 1) 
-	  {
-
-		echo "Der Eintrag wurde ge&auml;ndert<br>";
-		SetHeaderGo2Back();
-	
-          } 
-	  else 
-	  {
-		echo "Ein Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)";
-	  }
+	if (!IsSet($_GET["QID"]))
+		echo "\tFehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...";
+        else
+	{
+		$SQL = "UPDATE Questions SET Question=\"". $_GET["Question"]. 
+	  		 "\", AID=\"". $_SESSION['UID']. "\" , Answer=\"". $_GET["Answer"]. "\" ".
+			 "where QID = \"". $_GET["QID"]. "\" LIMIT 1";
+        	$Erg = mysql_query($SQL, $con);
+		if ($Erg == 1) 
+		{
+			echo "\tDer Eintrag wurde ge&auml;ndert<br>\n";
+			SetHeaderGo2Back();
+          	} 
+		else 
+			echo "\tEin Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)\n";
         }
 	break;
 
 case "transfer":
-	if (!IsSet($_GET["QID"])){
-?>
-	  Fehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...
-<?php
-        } else {
-	
+	if (!IsSet($_GET["QID"]))
+		echo "\tFehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...\n";
+	else
+	{
 		$SQL1="Select * from Questions where QID=". $_GET["QID"];
 		$Erg = mysql_query($SQL1, $con);
 		$SQL2="Insert into FAQ Values (\"\", \"".
 			mysql_result($Erg, 0, "Question")."\", \"".mysql_result($Erg, 0, "Answer")."\")";
 		$Erg = mysql_query($SQL2, $con);
-		if ($Erg == 1) {
-?>
-			Der Eintrag wurde &uuml;bertragen.<br>
-<?php
-	        } else {
-?>
-			Ein Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)
-<?php
-	        }
+		if ($Erg == 1) 
+			echo "\tDer Eintrag wurde &uuml;bertragen.<br>\n";
+	        else
+			echo "\tEin Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)\n";
 	}
 	
 	break;
@@ -164,45 +143,37 @@ case "transfer":
 // * je nach Übergabeoption ($quest) koennen FAQ's erfasst werden,
 // * geaendert oder geloscht werden...
 // *---------------------------------------------------------------------------
-
-
 case "faq":
 	$quest_bearb=0; // keine Fragenliste anzeigen, FAQ editieren...
-?>
-	FAQ-Liste:<br>
-	<a href="./faq.php?quest=faqnew">Neuen Eintrag</a>
-<?php
+	echo "\tFAQ-Liste:<br>";
+	echo "<a href=\"./faq.php?quest=faqnew\">Neuen Eintrag</a>";
+	
 	$SQL = "SELECT * FROM `FAQ`";
 	$Erg = mysql_query($SQL, $con);
 
 	// anzahl zeilen
 	$Zeilen  = mysql_num_rows($Erg);
 
-	for ($n = 0 ; $n < $Zeilen ; $n++) {
-	  if (mysql_result($Erg, $n, "Antwort")!="") {
-?>
-	      <p class='question'><?php echo mysql_result($Erg, $n, "Frage"); ?></p>
-	      <p class='answetion'><?php echo mysql_result($Erg, $n, "Antwort"); ?></p>
-              <a href="./faq.php?quest=faqedit&FAQID=<?php echo mysql_result($Erg, $n, "FID"); ?>">Bearbeiten</a>
-              <br>---<br>
-<?php 
-}
-	}
+	for ($n = 0 ; $n < $Zeilen ; $n++)
+		if (mysql_result($Erg, $n, "Antwort")!="")
+		{
+			echo "\t<p class=\"question\">".  mysql_result($Erg, $n, "Frage").  "</p>\n";
+			echo "\t<p class=\"answetion\">". mysql_result($Erg, $n, "Antwort")."</p>\n";
+			echo "\t<a href=\"./faq.php?quest=faqedit&FAQID=". mysql_result($Erg, $n, "FID"). 
+				"\">Bearbeiten</a>\n<br>---<br>\n";
+		}
 	break;
 
 case "faqedit":
        if (!IsSet($_GET["FAQID"]))
-       {
-?>
-	 Fehlerhafter Aufruf...<br>Bitte die Bearbeitung nochmals beginnen :)
-<?php
-	} else {
+		echo "\tFehlerhafter Aufruf...<br>Bitte die Bearbeitung nochmals beginnen :)\n";
+	else
+	{
+		$SQL = "SELECT * FROM FAQ where FID=". $_GET["FAQID"];
+		$Erg = mysql_query($SQL, $con);
 
-	$SQL = "SELECT * FROM FAQ where FID=". $_GET["FAQID"];
-	$Erg = mysql_query($SQL, $con);
-
-	// anzahl zeilen
-	$Zeilen  = mysql_num_rows($Erg);
+		// anzahl zeilen
+		$Zeilen  = mysql_num_rows($Erg);
 ?>
 	<form action="./faq.php" method="GET">
 		Frage:<br>
@@ -225,44 +196,30 @@ case "faqedit":
 
 case "faqdelete";
 	if (!IsSet($_GET["FAQID"]))
+		echo "\tFehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...\n";
+	else 
 	{
-?>
-		Fehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...
-<?php 
-	} else {
 		$SQL = "delete from FAQ where FID = \"". $_GET["FAQID"]. "\" LIMIT 1";
 		$Erg = mysql_query($SQL, $con);
-		if ($Erg == 1) {
-?>
-			Der Eintrag wurde gel&ouml;scht<br>
-<?php
-		} else {
-?>
-			Ein Fehler ist aufgetreten. Ist der Eintag bereits gel&ouml;scht gewesen?
-<?php
-		}
+		if ($Erg == 1) 
+			echo "\tDer Eintrag wurde gel&ouml;scht<br>\n";
+		else
+			echo "\tEin Fehler ist aufgetreten. Ist der Eintag bereits gel&ouml;scht gewesen?\n";
 	}
 	break;
 
 case "faqsave";
         if (!IsSet($_GET["FAQID"]))
+		echo "\tFehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...\n";
+        else 
 	{
-?>
-	  Fehlerhafter Aufruf... Bitte die Bearbeitung nochmal starten...
-<?php
-        } else {
-          $SQL = "UPDATE FAQ SET Frage=\"". $_GET["Frage"]. "\", Antwort=\"". $_GET["Antwort"]. 
-	  	 "\" where FID = \"". $_GET["FAQID"]. "\" LIMIT 1";
-          $Erg = mysql_query($SQL, $con);
-          if ($Erg == 1) {
-?>
-		Der Eintrag wurde ge&auml;ndert<br>
-<?php
-          } else {
-?>
-		Ein Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)
-<?php
-	  }
+        	$SQL = "UPDATE FAQ SET Frage=\"". $_GET["Frage"]. "\", Antwort=\"". $_GET["Antwort"]. 
+	  	 	"\" where FID = \"". $_GET["FAQID"]. "\" LIMIT 1";
+		$Erg = mysql_query($SQL, $con);
+		if ($Erg == 1) 
+			echo "\tDer Eintrag wurde ge&auml;ndert<br>\n";
+          	else
+			echo "\tEin Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)\n";
         }
 	break;
 
@@ -278,64 +235,49 @@ case "faqnew":
         </form>
 <?php
 	break;
+
 case "faqnewsave";
         $SQL = "INSERT INTO FAQ VALUES (\"\", \"". $_GET["Frage"]. "\", \"". $_GET["Antwort"]. "\")";
         $Erg = mysql_query($SQL, $con);
-        if ($Erg == 1) {
-?>
-		Der Eintrag wurde erfasst.<br>
-<?php
-        } else {
-?>
-		Ein Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)
-<?php
-	}
+        if ($Erg == 1)
+		echo "\tDer Eintrag wurde erfasst.<br>\n";
+        else
+		echo "\tEin Fehler ist aufgetreten. Sorry, du kannst es aber ja nochmal probieren :)\n";
         break;
 
-}
+} //switch ($_GET["quest"])
+
 
 // Hilfsroutine für die Anfragen:
 // Fragenliste anzeigen???
-
-if ($quest_bearb==1) {
-
+if ($quest_bearb==1) 
+{
 	$Erg = mysql_query($SQL, $con);
 	// anzahl zeilen
 	$Zeilen  = mysql_num_rows($Erg);
 
-	if ($Zeilen==0){
-?>
-		keine vorhanden...
-<?php 
-	} else {
-		for ($n = 0 ; $n < $Zeilen ; $n++) {
-?>
-<p>
-<?php echo nl2br(mysql_result($Erg, $n, "Question"))."\n"; ?>
-</p>
-			<br>
-			<a href="./faq.php?quest=edit&QID=<?php echo mysql_result($Erg, $n, "QID"); ?>">Bearbeiten</a>
-			<br>---<br>
-<?php
+	if ($Zeilen==0)
+		echo "\tkeine vorhanden...\n";
+	else
+		for ($n = 0 ; $n < $Zeilen ; $n++) 
+		{
+			echo "\t<p>". nl2br(mysql_result($Erg, $n, "Question"))."\n</p><br>\n";
+			echo "\t<a href=\"./faq.php?quest=edit&QID=". mysql_result($Erg, $n, "QID"). "\">Bearbeiten</a>\n";
+			echo "<br>---<br>\n";
 		}
-	}
-
 	
 }
 
-} else {
-
-?>
-Bitte w&auml;hle aus, ob du:
-
-<ul>
-	<li><a href="./faq.php?quest=all">Alle Anfragen anzeigen/bearbeiten m&ouml;chtest</a></li>
-	<li><a href="./faq.php?quest=open">Alle offenen Anfragen anzeigen/bearbeiten m&ouml;chtest</a></li>
-	<li><a href="./faq.php?quest=faq">Die FAQ's anzeigen/bearbeiten</a></li>
-</ul>
-
-<?php
-
+} //if (IsSet($_GET["quest"]))
+else 
+{
+	echo "Bitte w&auml;hle aus, ob du:\n";
+	echo "<ul>\n";
+	echo "\t<li><a href=\"./faq.php?quest=all\">Alle Anfragen anzeigen/bearbeiten m&ouml;chtest</a></li>\n";
+	echo "\t<li><a href=\"./faq.php?quest=open\">Alle offenen Anfragen anzeigen/bearbeiten m&ouml;chtest (". 
+		noAnswer(). ")</a></li>\n";
+	echo "\t<li><a href=\"./faq.php?quest=faq\">Die FAQ's anzeigen/bearbeiten</a></li>\n";
+	echo "</ul>\n";
 }
 
 include ("./inc/footer.php");
