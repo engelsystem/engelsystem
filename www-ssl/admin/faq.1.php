c72a2baa4256431345364003eeac07d3487266c8
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2006-12-01 21:49:52 +0000

    include wieder hinzugefuegt
    git-svn-id: svn://svn.cccv.de/engel-system@191 29ba0400-6e00-0410-a75a-ca02368028f8

--- www-ssl/admin/faq.1.php
+++ www-ssl/admin/faq.1.php
@@ -1,6 +1,6 @@
 <h4 class="menu"> Anfragen und FAQ </h4>
 
 <li><a href="faq.php?quest=all">Alle Anfragen</a></li>
-<li><a href="faq.php?quest=open">Offene Anfragen (<?PHP noAnswer(); ?>)</a></li>
+<li><a href="faq.php?quest=open">Offene Anfragen (<?PHP echo noAnswer(); ?>)</a></li>
 <li><a href="faq.php?quest=faq">FAQ-Liste editiern</a></li>
 
