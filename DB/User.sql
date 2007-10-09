546f80edd34be6e8d64bfb83df85bbbc309406ab
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2007-10-09 22:12:52 +0000

    User.sql aktualiesiert
    git-svn-id: svn://svn.cccv.de/engel-system@240 29ba0400-6e00-0410-a75a-ca02368028f8

--- DB/User.sql
+++ DB/User.sql
@@ -7,7 +7,7 @@
 --
 
 DROP TABLE IF EXISTS `User`;
-CREATE TABLE IF NOT EXISTS `User` (
+CREATE TABLE `User` (
   `UID` int(11) NOT NULL auto_increment,
   `Nick` varchar(23) NOT NULL default '',
   `Name` varchar(23) default NULL,
@@ -17,8 +17,8 @@ CREATE TABLE IF NOT EXISTS `User` (
   `DECT` varchar(4) default NULL,
   `Handy` varchar(40) default NULL,
   `email` varchar(123) default NULL,
-  `ICQ`  VARCHAR( 30 )  default NULL,
-  `jabber` VARCHAR( 200 ) default NULL,
+  `ICQ` varchar(30) default NULL,
+  `jabber` varchar(200) default NULL,
   `Size` varchar(4) default NULL,
   `Passwort` varchar(40) default NULL,
   `Gekommen` tinyint(4) NOT NULL default '0',
@@ -27,13 +27,15 @@ CREATE TABLE IF NOT EXISTS `User` (
   `color` tinyint(4) default '1',
   `Sprache` char(2) default 'EN',
   `Avatar` int(11) default '0',
+  `Menu` char(1) NOT NULL default 'L',
   `lastLogIn` datetime NOT NULL default '0000-00-00 00:00:00',
   `CreateDate` datetime NOT NULL default '0000-00-00 00:00:00',
   `Art` varchar(30) default NULL,
   `kommentar` text,
+  `Hometown` varchar(255) NOT NULL default '',
   PRIMARY KEY  (`UID`,`Nick`),
   UNIQUE KEY `Nick` (`Nick`)
-) TYPE=MyISAM AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=144 ;
 
 INSERT INTO `User` (`UID`, `Nick`, `Name`, `Vorname`, `Alter`, `Telefon`, `DECT`, `Handy`, `email`, `Size`, `Passwort`, `Gekommen`, `Aktiv`, `Tshirt`, `color`, `Sprache`, `Avatar`, `lastLogIn`, `Art`, `kommentar`) VALUES (1, 'admin', '', '', 0, '', '', '', '', '', '21232f297a57a5a743894a0e4a801fc3', 0, 0, 0, 6, 'EN', 115, '0000-00-00 00:00:00', '', '');
 
