d354d92b8a5801606d539d9806ebb6a72b0cbb4a
cookie <cookie@29ba0400-6e00-0410-a75a-ca02368028f8>
2007-10-09 21:56:36 +0000

    UserCVS.sql nei erstellt
    git-svn-id: svn://svn.cccv.de/engel-system@237 29ba0400-6e00-0410-a75a-ca02368028f8

--- DB/UserCVS.sql
+++ DB/UserCVS.sql
@@ -1,30 +1,28 @@
 -- phpMyAdmin SQL Dump
--- version 2.6.2-Debian-3sarge1
+-- version 2.9.1.1-Debian-4
 -- http://www.phpmyadmin.net
---
+-- 
 -- Host: localhost
--- Erstellungszeit: 02. Dezember 2006 um 09:28
--- Server Version: 4.0.24
--- PHP-Version: 4.3.10-16
---
+-- Erstellungszeit: 09. Oktober 2007 um 21:53
+-- Server Version: 5.0.32
+-- PHP-Version: 5.2.0-8+etch7
+-- 
 -- Datenbank: `Himmel`
---
+-- 
 
 -- --------------------------------------------------------
 
---
+-- 
 -- Tabellenstruktur für Tabelle `UserCVS`
---
+-- 
 
-DROP TABLE IF EXISTS `UserCVS`;
 CREATE TABLE `UserCVS` (
   `UID` int(11) NOT NULL default '0',
-  `index.php` char(1) NOT NULL default 'Y',
+  `index.php` char(1) NOT NULL default 'N',
   `logout.php` char(1) NOT NULL default 'Y',
-  `faq.php` char(1) NOT NULL default 'Y',
-  `lageplan.php` char(1) NOT NULL default 'Y',
+  `faq.php` char(1) NOT NULL default 'N',
+  `lageplan.php` char(1) NOT NULL default 'N',
   `makeuser.php` char(1) NOT NULL default 'Y',
-
   `nonpublic/index.php` char(1) NOT NULL default 'Y',
   `nonpublic/news.php` char(1) NOT NULL default 'Y',
   `nonpublic/newsAddMeting` char(1) NOT NULL default 'N',
@@ -33,7 +31,7 @@ CREATE TABLE `UserCVS` (
   `nonpublic/engelbesprechung.php` char(1) NOT NULL default 'Y',
   `nonpublic/schichtplan.php` char(1) NOT NULL default 'Y',
   `nonpublic/schichtplan_add.php` char(1) NOT NULL default 'Y',
-  `nonpublic/schichtplan_beamer.php` char(1) NOT NULL default 'Y',
+  `nonpublic/schichtplan_beamer.php` char(1) NOT NULL default 'N',
   `nonpublic/wecken.php` char(1) NOT NULL default 'N',
   `nonpublic/waeckliste.php` char(1) NOT NULL default 'N',
   `nonpublic/messages.php` char(1) NOT NULL default 'Y',
@@ -48,6 +46,7 @@ CREATE TABLE `UserCVS` (
   `admin/user.php` char(1) NOT NULL default 'N',
   `admin/user2.php` char(1) NOT NULL default 'N',
   `admin/userDefaultSetting.php` char(1) NOT NULL default 'N',
+  `admin/UserPicture.php` char(1) NOT NULL default 'N',
   `admin/aktiv.php` char(1) NOT NULL default 'N',
   `admin/tshirt.php` char(1) NOT NULL default 'N',
   `admin/news.php` char(1) NOT NULL default 'N',
@@ -55,14 +54,20 @@ CREATE TABLE `UserCVS` (
   `admin/free.php` char(1) NOT NULL default 'N',
   `admin/sprache.php` char(1) NOT NULL default 'N',
   `admin/dect.php` char(1) NOT NULL default 'N',
+  `admin/dect_call.php` char(1) NOT NULL default 'N',
   `admin/dbUpdateFromXLS.php` char(1) NOT NULL default 'N',
   `admin/Recentchanges.php` char(1) NOT NULL default 'N',
   `admin/debug.php` char(1) NOT NULL default 'N',
+  `Herald` char(1) NOT NULL default 'N',
+  `Info` char(1) NOT NULL default 'N',
+  `Conference` char(1) NOT NULL default 'N',
   PRIMARY KEY  (`UID`)
-) TYPE=MyISAM;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
---
+-- 
 -- Daten für Tabelle `UserCVS`
---
+-- 
 
-INSERT INTO `UserCVS` (`UID`, `index.php`, `logout.php`, `faq.php`, `lageplan.php`, `makeuser.php`, `nonpublic/index.php`, `nonpublic/news.php`, `nonpublic/newsAddMeting`, `nonpublic/news_comments.php`, `nonpublic/myschichtplan.php`, `nonpublic/engelbesprechung.php`, `admin/index.php`, `nonpublic/schichtplan.php`, `nonpublic/schichtplan_add.php`, `nonpublic/schichtplan_beamer.php`, `nonpublic/wecken.php`, `nonpublic/waeckliste.php`, `nonpublic/faq.php`, `nonpublic/einstellungen.php`, `admin/debug.php`, `admin/dbUpdateFromXLS.php`, `admin/room.php`, `admin/EngelType.php`, `admin/schichtplan.php`, `admin/shiftadd.php`, `admin/schichtplan_druck.php`, `admin/userDefaultSetting.php`, `admin/user.php`, `admin/user2.php`, `admin/aktiv.php`, `admin/tshirt.php`, `admin/news.php`, `admin/faq.php`, `admin/free.php`, `admin/sprache.php`, `admin/dect.php`) VALUES (-1, 'Y', 'N', 'N', 'N', 'N', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N'), (1, 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO `UserCVS` (`UID`, `index.php`, `logout.php`, `faq.php`, `lageplan.php`, `makeuser.php`, `nonpublic/index.php`, `nonpublic/news.php`, `nonpublic/newsAddMeting`, `nonpublic/news_comments.php`, `nonpublic/myschichtplan.php`, `nonpublic/engelbesprechung.php`, `nonpublic/schichtplan.php`, `nonpublic/schichtplan_add.php`, `nonpublic/schichtplan_beamer.php`, `nonpublic/wecken.php`, `nonpublic/waeckliste.php`, `nonpublic/messages.php`, `nonpublic/faq.php`, `nonpublic/einstellungen.php`, `admin/index.php`, `admin/room.php`, `admin/EngelType.php`, `admin/schichtplan.php`, `admin/shiftadd.php`, `admin/schichtplan_druck.php`, `admin/user.php`, `admin/user2.php`, `admin/userDefaultSetting.php`, `admin/UserPicture.php`, `admin/aktiv.php`, `admin/tshirt.php`, `admin/news.php`, `admin/faq.php`, `admin/free.php`, `admin/sprache.php`, `admin/dect.php`, `admin/dect_call.php`, `admin/dbUpdateFromXLS.php`, `admin/Recentchanges.php`, `admin/debug.php`, `Herald`, `Info`, `Conference`) VALUES 
+(-1, 'Y', 'N', 'N', 'N', 'Y', 'Y', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N', 'N'),
+(1, 'N', 'Y', 'N', 'N', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'N', 'N', 'N', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'Y', 'N');
