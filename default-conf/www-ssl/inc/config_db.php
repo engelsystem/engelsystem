a1fdd669eeb85d466ae0e4d0389d664246912c18
ichdasich <ichdasich@29ba0400-6e00-0410-a75a-ca02368028f8>
2008-04-09 03:49:12 +0000

    minor changes for installscript comp.
    git-svn-id: svn://svn.cccv.de/engel-system@269 29ba0400-6e00-0410-a75a-ca02368028f8

--- default-conf/www-ssl/inc/config_db.php
+++ default-conf/www-ssl/inc/config_db.php
@@ -1,6 +1,6 @@
 <?php
 include("./inc/funktion_db.php");
 
-$con = mysql_connect("localhost", "user", "pass") or die ("connection failed");
+$con = mysql_connect("localhost", "root", "changeme") or die ("connection failed");
 $sel = mysql_select_db("tabel") or die (mysql_error());
 ?>
