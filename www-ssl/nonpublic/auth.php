e1146179aea6fff0908b693b10cf098b6dfa3219
root <root@www.engelsystem.de>
2010-12-29 14:31:53 +0100

    change include

--- www-ssl/nonpublic/auth.php
+++ www-ssl/nonpublic/auth.php
@@ -1,8 +1,8 @@
 <?PHP
 header('Content-Type: application/json');
 
-include ("../../includes/config.php");
-include ("../../includes/config_db.php");
+include ("../../../27c3/includes/config.php");
+include ("../../../27c3/includes/config_db.php");
 
 $User = $_POST['user'];
 $Pass = $_POST['pw'];
