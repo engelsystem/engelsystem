bf64254625123ecd36662628862442ccf7671699
Philip Häusler <msquare@notrademark.de>
2011-06-01 15:37:41 +0200

    fixed menu and credits link

--- includes/funktion_menu.php
+++ includes/funktion_menu.php
@@ -1,41 +1,42 @@
 <?php
-  function ShowMenu( $MenuName) {
-    global $MenueTableStart, $MenueTableEnd, $_SESSION, $debug, $url, $ENGEL_ROOT;
-    $Gefunden = false;
 
-    // Ueberschift
-    $Text = "<h4 class=\"menu\">". Get_Text("$MenuName/"). "</h4><ul>";
 
-    // Eintraege
-    foreach($_SESSION['CVS'] as $Key => $Entry)
-      if( strpos( $Key, ".php") > 0)
-        if( (strpos( "00$Key", "0$MenuName") > 0) ||
-          ((strlen($MenuName)==0) && (strpos( "0$Key", "/") == 0) ) ) {
-          $TempName = Get_Text($Key, true);
+// Men√º generieren
+function ShowMenu($MenuName) {
+	global $MenueTableStart, $MenueTableEnd, $_SESSION, $debug, $url, $ENGEL_ROOT;
+	$Gefunden = false;
 
-          if(( true||$debug) && (strlen($TempName)==0) )
-            $TempName = "not found: \"$Key\"";
+	// Ueberschift
+	$Text = "<h4 class=\"menu\">" . Get_Text("$MenuName/") . "</h4><ul>";
 
-          if( $Entry == "Y") {
-            //zum absichtlkichen ausblenden von eintr‰gen
-            if( strlen($TempName)>1) {
-              //sonderf‰lle:
+	// Eintraege
+	foreach ($_SESSION['CVS'] as $Key => $Entry)
+		if (strpos($Key, ".php") > 0)
+			if ((strpos("00$Key", "0$MenuName") > 0) || ((strlen($MenuName) == 0) && (strpos("0$Key", "/") == 0))) {
+				$TempName = Get_Text($Key, true);
 
-              if( $Key=="admin/faq.php")
-                $TempName .= " (". noAnswer(). ")";
-            //ausgabe
-            $Text .= "\t\t\t<li><a href=\"". $url. $ENGEL_ROOT. $Key. "\">$TempName</a></li>\n";
-            $Gefunden = true;
-          }
-        }
-        elseif( $debug ) 
-        {
-          $Gefunden = true;
-          $Text .= "\t\t\t<li>$TempName ($Key)</li>\n";
-        }
-      }
-  if( $Gefunden)
-    echo $MenueTableStart.$Text.$MenueTableEnd;
-}//function ShowMenue
+				if ((true || $debug) && (strlen($TempName) == 0))
+					$TempName = "not found: \"$Key\"";
 
+				if ($Entry == "Y") {
+					//zum absichtlkichen ausblenden von eintr√§gen
+					if (strlen($TempName) > 1) {
+						//sonderf√§lle:
+
+						if ($Key == "admin/faq.php")
+							$TempName .= " (" . noAnswer() . ")";
+						elseif ($Key == "credits.php") continue;
+						//ausgabe
+						$Text .= "\t\t\t<li><a href=\"" . $url . $ENGEL_ROOT . $Key . "\">$TempName</a></li>\n";
+						$Gefunden = true;
+					}
+				}
+				elseif ($debug) {
+					$Gefunden = true;
+					$Text .= "\t\t\t<li>$TempName ($Key)</li>\n";
+				}
+			}
+	if ($Gefunden)
+		echo $MenueTableStart . $Text . $MenueTableEnd;
+} //function ShowMenue
 ?>
