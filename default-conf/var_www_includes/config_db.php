b16d0abb6ec9063aab239c291ff91ac5640b8d7d
Philip Häusler <msquare@notrademark.de>
2011-06-02 16:59:18 +0200

    db config

--- default-conf/var_www_includes/config_db.php
+++ default-conf/var_www_includes/config_db.php
@@ -1,8 +1,9 @@
 <?php
-include ("../include/funktion_db.php");
 
-$con = mysql_connect("localhost", "root", "changeme") or die("connection failed");
-$sel = mysql_select_db("tabel") or die(mysql_error());
-mysql_query("SET CHARACTER SET utf8;", $con);
-mysql_query("SET NAMES 'utf8'", $con);
+
+// MySQL-Connection Settings
+$config['host'] = "localhost";
+$config['user'] = "root";
+$config['pw'] = "";
+$config['db'] = "engelsystem";
 ?>
