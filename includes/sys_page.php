5113776a42d0bc7ff8ed2db8619045d3ffb164a9
Daniel Friesel <derf@finalrewind.org>
2011-06-03 11:09:25 +0200

    include/pages/admin_faq cleanup

--- includes/sys_page.php
+++ includes/sys_page.php
@@ -8,6 +8,14 @@ function strip_request_item($name) {
 	);
 }
 
+function strip_request_item_nl($name) {
+	return preg_replace(
+		"/([^\p{L}\p{P}\p{Z}\p{N}\n]{1,})/ui",
+		'',
+		strip_tags($_REQUEST[$name])
+	);
+}
+
 function error($msg) {
 	return '<p class="error">' . $msg . '</p>';
 }
