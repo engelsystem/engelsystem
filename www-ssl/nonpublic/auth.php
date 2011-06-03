f38500a2f10d4e1bfe4f7a40405043cefc05644f
Philip Häusler <msquare@notrademark.de>
2011-06-03 15:31:59 +0200

    delete old json auth service

--- www-ssl/nonpublic/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// Bleibt erstmal, damit Benutzer, die die Schnittstelle nutzen mitkriegen, dass diese Umgezogen ist
-echo json_encode(array (
-	'status' => 'failed',
-	'error' => "JSON Service moved to https://engelsystem.de/?auth&user=<user>&pw=<password>&so=<key>"
-));
-?>
