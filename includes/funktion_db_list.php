ded7bcb0b49e101c0414034fe1ff168a043b6a75
Philip Häusler <msquare@notrademark.de>
2011-06-03 14:48:05 +0200

    debug -> admin log

--- includes/funktion_db_list.php
+++ includes/funktion_db_list.php
@@ -1,64 +1,62 @@
 <?php
-  function funktion_db_list($Table_Name) {
-    global $con;
 
-    $SQL = "SELECT * FROM `".$Table_Name."`";
-    $Erg = mysql_query($SQL, $con);
 
-  // anzahl zeilen
-  $Zeilen  = mysql_num_rows($Erg);
+// Gibt eine Tabelle generisch als HTML aus
+function funktion_db_list($Table_Name) {
+	global $con;
 
-  $Anzahl_Felder = mysql_num_fields($Erg);
-
-  echo "<table class=\"border\" cellpadding=\"2\" cellspacing=\"1\">";
-  echo "<caption>DB: $Table_Name</caption>";
-
-  echo "<tr class=\"contenttopic\">";
-  for ($m = 0 ; $m < $Anzahl_Felder ; $m++)
-  {
-    echo "<th>". mysql_field_name($Erg, $m). "</th>";
-  }
-  echo "</tr>";
-
-  for ($n = 0 ; $n < $Zeilen ; $n++)
-  {
-    echo "<tr class=\"content\">";
-    for ($m = 0 ; $m < $Anzahl_Felder ; $m++)
-    {
-      echo "<td>".mysql_result($Erg, $n, $m). "</td>"; 
-    }
-    echo "</tr>";
-  }
-  echo "</table>";
-}
-
-function funktion_db_element_list_2row( $TopicName, $SQL) 
-{
 	$html = "";
-  $html .= "<table class=\"border\" cellpadding=\"2\" cellspacing=\"1\">\n";
-  $html .= "<caption>$TopicName</caption>";
-#  $html .= "<tr class=\"contenttopic\"> <td><h1>$TopicName</h1></td> </tr>\n";
 
-  $Erg = sql_query($SQL);
-  
-  $html .= "<tr class=\"contenttopic\">";
-  for ($m = 0 ; $m < mysql_num_fields($Erg) ; $m++)
-  {
-    $html .= "<th>". mysql_field_name($Erg, $m). "</th>";
-  }
-  $html .= "</tr>";
+	$SQL = "SELECT * FROM `" . $Table_Name . "`";
+	$Erg = mysql_query($SQL, $con);
 
-  for ($n = 0 ; $n < mysql_num_rows($Erg) ; $n++)
-  {
-    $html .= "<tr class=\"content\">";
-    for ($m = 0 ; $m < mysql_num_fields($Erg) ; $m++)
-    {
-      $html .= "<td>".mysql_result($Erg, $n, $m). "</td>"; 
-    }
-    $html .= "</tr>";
-  }
-  $html .= "</table>\n";
-  return $html;
+	// anzahl zeilen
+	$Zeilen = mysql_num_rows($Erg);
+
+	$Anzahl_Felder = mysql_num_fields($Erg);
+
+	$html .= "<table class=\"border\" cellpadding=\"2\" cellspacing=\"1\">";
+	$html .= "<caption>DB: $Table_Name</caption>";
+
+	$html .= "<tr class=\"contenttopic\">";
+	for ($m = 0; $m < $Anzahl_Felder; $m++) {
+		$html .= "<th>" . mysql_field_name($Erg, $m) . "</th>";
+	}
+	$html .= "</tr>";
+
+	for ($n = 0; $n < $Zeilen; $n++) {
+		$html .= "<tr class=\"content\">";
+		for ($m = 0; $m < $Anzahl_Felder; $m++) {
+			$html .= "<td>" . mysql_result($Erg, $n, $m) . "</td>";
+		}
+		$html .= "</tr>";
+	}
+	$html .= "</table>";
+	return $html;
 }
 
+function funktion_db_element_list_2row($TopicName, $SQL) {
+	$html = "";
+	$html .= "<table class=\"border\" cellpadding=\"2\" cellspacing=\"1\">\n";
+	$html .= "<caption>$TopicName</caption>";
+	#  $html .= "<tr class=\"contenttopic\"> <td><h1>$TopicName</h1></td> </tr>\n";
+
+	$Erg = sql_query($SQL);
+
+	$html .= "<tr class=\"contenttopic\">";
+	for ($m = 0; $m < mysql_num_fields($Erg); $m++) {
+		$html .= "<th>" . mysql_field_name($Erg, $m) . "</th>";
+	}
+	$html .= "</tr>";
+
+	for ($n = 0; $n < mysql_num_rows($Erg); $n++) {
+		$html .= "<tr class=\"content\">";
+		for ($m = 0; $m < mysql_num_fields($Erg); $m++) {
+			$html .= "<td>" . mysql_result($Erg, $n, $m) . "</td>";
+		}
+		$html .= "</tr>";
+	}
+	$html .= "</table>\n";
+	return $html;
+}
 ?>
--- includes/pages/admin_log.php
+++ includes/pages/admin_log.php
@@ -1,5 +1,7 @@
 <?php
 function admin_log() {
+	require_once ("includes/funktion_db_list.php");
+
 	$html = "";
 	$SQL = "SELECT * FROM `ChangeLog` ORDER BY `Time` DESC LIMIT 0,10000";
 	$Erg = sql_query($SQL);
@@ -19,6 +21,55 @@ function admin_log() {
 	} else {
 		$html .= "Log is empty...";
 	}
+	$html .= "<hr />";
+
+	$html .= "<h1>Web Counter</h1>";
+	$html .= funktion_db_list("Counter");
+
+	/*
+	$html .= "<h1>Raeume</h1> <br />";
+	funktion_db_list("Raeume");
+	
+	$html .= "<h1>Schichtbelegung</h1> <br />";
+	funktion_db_list("Schichtbelegung");
+	
+	$html .= "<h1>Schichtplan</h1> <br />Hier findest du alle bisher eingetragenen Schichten:";
+	funktion_db_list("Schichtplan");
+	
+	$html .= "<h1>User</h1> <br />";
+	funktion_db_list("User");
+	
+	$html .= "<h1>News</h1> <br />";
+	funktion_db_list("News");
+	
+	$html .= "<h1>FAQ</h1> <br />";
+	funktion_db_list("FAQ");
+	
+	$html .= "Deaktiviert";
+	*/
+
+	$html .= "<hr>\n";
+	$html .= funktion_db_element_list_2row("Tshirt-Size aller engel", "SELECT `Size`, COUNT(`Size`) FROM `User` GROUP BY `Size`");
+	$html .= "<br />\n";
+	$html .= funktion_db_element_list_2row("Tshirt ausgegeben", "SELECT `Size`, COUNT(`Size`) FROM `User` WHERE `Tshirt`='1' GROUP BY `Size`");
+	$html .= "<br />\n";
+	$html .= funktion_db_element_list_2row("Tshirt nicht  ausgegeben (Gekommen=1)", "SELECT COUNT(`Size`), `Size` FROM `User` WHERE `Gekommen`='1' and `Tshirt`='0' GROUP BY `Size`");
+
+	$html .= "<hr>\n";
+	$html .= funktion_db_element_list_2row("Hometown", "SELECT COUNT(`Hometown`), `Hometown` FROM `User` GROUP BY `Hometown`");
+	$html .= "<br />\n";
+	$html .= funktion_db_element_list_2row("Engeltypen", "SELECT COUNT(`Art`), `Art` FROM `User` GROUP BY `Art`");
+
+	$html .= "<hr>\n";
+	$html .= funktion_db_element_list_2row("Gesamte Arbeit", "SELECT COUNT(*) AS `Count [x]`, SUM(Shifts.Len) as `Sum [h]` from Shifts LEFT JOIN ShiftEntry USING(SID)");
+	$html .= "<br />\n";
+	$html .= funktion_db_element_list_2row("Geleisteter Arbeit", "SELECT COUNT(*) AS `Count [x]`, SUM(Shifts.Len) as `Sum [h]` from Shifts LEFT JOIN ShiftEntry USING(SID) WHERE (ShiftEntry.UID!=0)");
+
+	$html .= "<hr>\n";
+	$html .= funktion_db_element_list_2row("Gesamte Arbeit (Ohne Raum aufabau (RID=7)", "SELECT COUNT(*) AS `Count [x]`, SUM(Shifts.Len) as `Sum [h]` from Shifts LEFT JOIN ShiftEntry USING(SID) WHERE (Shifts.RID!=7)");
+	$html .= "<br />\n";
+	$html .= funktion_db_element_list_2row("Geleisteter Arbeit (Ohne Raum aufabau (RID=7)", "SELECT COUNT(*) AS `Count [x]`, SUM(Shifts.Len) as `Sum [h]` from Shifts LEFT JOIN ShiftEntry USING(SID) WHERE (ShiftEntry.UID!=0) AND (Shifts.RID!=7)");
+
 	return $html;
 }
 ?>
