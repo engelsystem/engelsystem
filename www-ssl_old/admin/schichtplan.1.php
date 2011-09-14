d194783e8f2aa2e9e27e34572cda6f796930ac37
Philip Häusler <msquare@notrademark.de>
2011-09-14 22:54:36 +0200

    cleanup from old files

--- www-ssl_old/admin/schichtplan.1.php
+++
@@ -1,5 +0,0 @@
-<h4>Schichtplan</h4>
-<ul class="content">
-<li><a href="./schichtplan.php">Ein-/Austragen</a></li>
-<li><a href="./schichtplan_druck.php">Druckversion</a></li>
-</ul>
--- www-ssl_old/admin/schichtplan_druck.1.php
+++
@@ -1,5 +0,0 @@
-<h4 class="menu"> Schichtplan </h4>
-
-<li><a href="./schichtplan.php">Ein-/Austragen</a></li>
-<li><a href="./schichtplan_druck.php">Druckversion</a></li>
-
