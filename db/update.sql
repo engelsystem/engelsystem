86a72bd934c408ad558368a6b4c0fcb9cc8e7892
Philip Häusler <msquare@notrademark.de>
2011-10-11 23:43:44 +0200

    update sql fix

--- db/update.sql
+++ db/update.sql
@@ -1,8 +1,6 @@
 /* Update für #27, iCal-Export */
-ALTER TABLE `User` ADD `ical_key` VARCHAR( 32 ) NOT NULL ,
-ADD UNIQUE (
-`ical_key`
-);
+ALTER TABLE `User` ADD `ical_key` VARCHAR( 32 ) NOT NULL;
+ALTER TABLE `User` ADD INDEX ( `ical_key` ) 
 
 INSERT INTO `engelsystem`.`Privileges` (
 `id` ,
