26ffee4a96f08aa58a348d11674f6fd29c1498b0
Philip Häusler <msquare@notrademark.de>
2012-12-27 02:18:56 +0100

    #28 logging doku

--- includes/model/LogEntries_model.php
+++ includes/model/LogEntries_model.php
@@ -11,6 +11,9 @@ function LogEntry_create($nick, $message) {
   sql_query("INSERT INTO `LogEntries` SET `timestamp`=" . sql_escape($timestamp) . ", `nick`='" . sql_escape($nick) . "', `message`='" . sql_escape($message) . "'");
 }
 
+/**
+ * Returns log entries of the last 24 hours with maximum count of 1000.
+ */
 function LogEntries() {
   $log_entries_source = sql_select("SELECT * FROM `LogEntries` WHERE `timestamp` > " . (time() - 24*60*60) . " ORDER BY `timestamp` DESC LIMIT 1000");
   return $log_entries_source;
