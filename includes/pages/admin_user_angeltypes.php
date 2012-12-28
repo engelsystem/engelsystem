f759296db3277abb6416349e6c5de15a7db6bbcc
Philip Häusler <msquare@notrademark.de>
2012-12-28 17:54:34 +0100

    make new angeltype confirmes less annoying

--- includes/pages/admin_user_angeltypes.php
+++ includes/pages/admin_user_angeltypes.php
@@ -96,13 +96,13 @@ function admin_user_angeltypes() {
 }
 
 /**
- * Anzeige, ob noch Engeltypen bestätigt werden müssen. Damit werden Erzengel auf jeder Seite im Kopfbereich "genervt".
+ * Anzeige, ob noch Engeltypen bestätigt werden müssen. Damit werden Erzengel auf jeder Seite im Kopfbereich "genervt", wenn zu ihren Aufgaben noch Engel bestätigt werden müssen.
  */
 function admin_new_user_angeltypes() {
   global $user, $privileges;
 
   if (in_array("admin_user_angeltypes", $privileges)) {
-    $unconfirmed_angeltypes = sql_num_query("SELECT * FROM `UserAngelTypes` JOIN `AngelTypes` ON `UserAngelTypes`.`angeltype_id`=`AngelTypes`.`id` WHERE `AngelTypes`.`restricted`=1 AND `UserAngelTypes`.`confirm_user_id` IS NULL LIMIT 1") > 0;
+    $unconfirmed_angeltypes = sql_num_query("SELECT * FROM `UserAngelTypes` JOIN `AngelTypes` ON `UserAngelTypes`.`angeltype_id`=`AngelTypes`.`id` WHERE `UserAngelTypes`.`angeltype_id` IN (SELECT `angeltype_id` FROM `UserAngelTypes` WHERE `user_id`=" . sql_escape($user['UID']) . ") AND `AngelTypes`.`restricted`=1 AND `UserAngelTypes`.`confirm_user_id` IS NULL LIMIT 1") > 0;
 
     if ($unconfirmed_angeltypes)
       return info('<a href="' . page_link_to('admin_user_angeltypes') . '">There are unconfirmed angeltypes!</a>', true);
