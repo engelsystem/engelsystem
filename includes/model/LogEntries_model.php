be1bb5fbeb2cb93105b12cc9d9b4cae676f59a7f
Felix Favre <gnomus@gnomus.de>
2014-12-15 21:23:57 +0100

    add simple search to log view

--- includes/model/LogEntries_model.php
+++ includes/model/LogEntries_model.php
@@ -18,4 +18,11 @@ function LogEntries() {
   return sql_select("SELECT * FROM `LogEntries` ORDER BY `timestamp` DESC LIMIT 10000");
 }
 
+/**
+ * Returns log entries filtered by a keyword
+ */
+function LogEntries_filter($keyword) {
+  return sql_select("SELECT * FROM `LogEntries` WHERE `nick` LIKE '%" . sql_escape($keyword) . "%' OR `message` LIKE '%" . sql_escape($keyword) . "%' ORDER BY `timestamp` DESC");
+}
+
 ?>
--- includes/pages/admin_log.php
+++ includes/pages/admin_log.php
@@ -4,20 +4,32 @@ function admin_log_title() {
 }
 
 function admin_log() {
-  $log_entries_source = LogEntries();
+
+  if (isset($_POST['keyword'])) {
+    $filter = $_POST['keyword'];
+    $log_entries_source = LogEntries_filter($_POST['keyword']);
+  } else {
+    $filter = "";
+    $log_entries_source = LogEntries();
+  }
+
   $log_entries = array();
   foreach ($log_entries_source as $log_entry) {
     $log_entry['date'] = date("d.m.Y H:i", $log_entry['timestamp']);
     $log_entries[] = $log_entry;
   }
-  
+
   return page_with_title(admin_log_title(), array(
       msg(),
+      form(array(
+        form_text('keyword', _("Suche"), $filter),
+        form_submit(_("Suche"), "Go")
+      )),
       table(array(
           'date' => "Time",
           'nick' => "Angel",
-          'message' => "Log Entry" 
-      ), $log_entries) 
+          'message' => "Log Entry"
+      ), $log_entries)
   ));
 }
 ?>
