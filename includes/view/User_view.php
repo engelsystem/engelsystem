c18c0983fc35ff27bfa038f96fdf465849ee63f0
Felix Favre <gnomus@gnomus.de>
2014-12-15 21:30:52 +0100

    only show "arrived" button if user has rights for it
    fixes #172

--- includes/view/User_view.php
+++ includes/view/User_view.php
@@ -184,7 +184,7 @@ function User_view($user_source, $admin_user_privilege, $freeloader, $user_angel
           div('col-md-12', array(
               buttons(array(
                   $admin_user_privilege ? button(page_link_to('admin_user') . '&id=' . $user_source['UID'], glyph("edit") . _("edit")) : '',
-                  ! $user_source['Gekommen'] ? button(page_link_to('admin_arrive') . '&arrived=' . $user_source['UID'], _("arrived")) : '',
+                  ($admin_user_privilege && !$user_source['Gekommen']) ? button(page_link_to('admin_arrive') . '&arrived=' . $user_source['UID'], _("arrived")) : '',
                   $its_me ? button(page_link_to('user_settings'), glyph('list-alt') . _("Settings")) : '',
                   $its_me ? button(page_link_to('ical') . '&key=' . $user_source['api_key'], glyph('calendar') . _("iCal Export")) : '',
                   $its_me ? button(page_link_to('shifts_json_export') . '&key=' . $user_source['api_key'], glyph('export') . _("JSON Export")) : '',
