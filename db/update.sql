a30af6e46bf536442635094f6add54154c4048be
Philip Häusler <msquare@notrademark.de>
2015-08-14 12:37:08 +0200

    allow up to 50 chars angeltype names (25 before)

--- db/update.sql
+++ db/update.sql
@@ -1,3 +1,6 @@
+/* Allow longer angeltype names */
+ALTER TABLE `AngelTypes` CHANGE `name` `name` VARCHAR( 50 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '';
+
 /* Enable multiple vouchers */
 ALTER TABLE `User` CHANGE `got_voucher` `got_voucher` INT NOT NULL;
 
