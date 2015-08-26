8c1f6d2c9c36d84bff629e4bd1e1fdbbe4b83d76
Felix Favre <felix.favre@protonet.info>
2015-08-26 13:33:38 +0200

    removed sessionhelper. we should let this handle the server administrator within the PHP config

--- includes/engelsystem_provider.php
+++ includes/engelsystem_provider.php
@@ -76,9 +76,6 @@ require_once realpath(__DIR__ . '/../includes/pages/user_shifts.php');
 
 require_once realpath(__DIR__ . '/../vendor/parsedown/Parsedown.php');
 
-if (! defined('PHPUNIT_TESTSUITE')) {
-  session_lifetime(24 * 60, preg_replace("/[^a-z0-9-]/", '', md5(__DIR__)));
-}
 session_start();
 
 gettext_init();
