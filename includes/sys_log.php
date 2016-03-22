d0a07a74cc0323d6fcc26c411ec272bc9dbee135
Philip Häusler <msquare@notrademark.de>
2016-03-22 14:41:32 +0100

    remove preg replace with e flag for php 7

--- includes/sys_log.php
+++ includes/sys_log.php
@@ -32,7 +32,7 @@ function debug_string_backtrace() {
   $trace = preg_replace('/^#0\s+' . __FUNCTION__ . "[^\n]*\n/", '', $trace, 1);
   
   // Renumber backtrace items.
-  $trace = preg_replace('/^#(\d+)/me', '\'#\' . ($1 - 1)', $trace);
+  // $trace = preg_replace('/^#(\d+)/me', '\'#\' . ($1 - 1)', $trace);
   
   return $trace;
 }
