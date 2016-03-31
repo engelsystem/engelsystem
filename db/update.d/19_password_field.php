e6c2532aad9801a710d595ddb25a345521247fc8
kamishetty sreeja <sreeja25kamishetty@gmail.com>
2016-03-31 18:45:39 +0530

    Deleted update.php and update.d directory

--- db/update.d/19_password_field.php
+++
@@ -1,7 +0,0 @@
-<?php
-// make the Passwort column in the User table longer to store more advanced hashes with salts
-$res = sql_select("DESCRIBE `User` `Passwort`");
-if ($res[0]['Type'] == 'varchar(40)') {
-	sql_query("ALTER TABLE `User` CHANGE `Passwort` `Passwort` VARCHAR(128) NULL");
-	$applied = true;
-}
