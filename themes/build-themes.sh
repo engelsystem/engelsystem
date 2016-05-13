6a00cd815cffb4bba27425ce6c85bb83038166e0
Dishant Khanna <dishant_khanna@outlook.com>
2016-05-13 17:02:00 +0530

    update build-themes.sh for theme4.css

--- themes/build-themes.sh
+++ themes/build-themes.sh
@@ -4,3 +4,4 @@ lessc theme0.less > ../public/css/theme0.css
 lessc theme1.less > ../public/css/theme1.css
 lessc theme2.less > ../public/css/theme2.css
 lessc theme3.less > ../public/css/theme3.css
+lessc theme4.less > ../public/css/theme4.css
