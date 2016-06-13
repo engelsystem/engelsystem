af35f160ba8cfd12ddb05a21c4753479bd04462d
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-06-13 12:14:38 +0530

    Added 'Events' table

--- db/update.sql
+++ db/update.sql
@@ -1,6 +1,6 @@
 -- drivers license information
 CREATE TABLE IF NOT EXISTS `UserDriverLicenses` (
-  `user_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `has_car` tinyint(1) NOT NULL,
   `has_license_car` tinyint(1) NOT NULL,
   `has_license_3_5t_transporter` tinyint(1) NOT NULL,
@@ -12,11 +12,12 @@ CREATE TABLE IF NOT EXISTS `UserDriverLicenses` (
 ALTER TABLE `UserDriverLicenses`
   ADD CONSTRAINT `userdriverlicenses_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`UID`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+-- -----------------------------------------------------------------------------
+-- Update table 'Angeltypes'
 ALTER TABLE `AngelTypes` ADD `requires_driver_license` BOOLEAN NOT NULL;
 
 -- -----------------------------------------------------------------------------
-
-
+-- Update 'User' Table
 ALTER TABLE `User`
   ADD UNIQUE (email),
   ADD `current_city` varchar(255) DEFAULT NULL,
@@ -27,3 +28,15 @@ ALTER TABLE `User`
   ADD `organization_web` varchar(255) DEFAULT NULL;
 
 -- -----------------------------------------------------------------------------  
+-- Events information table
+CREATE TABLE IF NOT EXISTS `Events` (
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `organizer` varchar(255) NOT NULL,
+  `event_date` int(11) NOT NULL,
+  `event_time` int(11) NOT NULL,
+  `venue` varchar(255) NOT NULL,
+  PRIMARY KEY (`event_id`),
+  UNIQUE KEY `Name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
