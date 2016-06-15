164f0a1a5a436fadea0e73a8597c96e9a534fe18
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-06-15 13:11:59 +0530

    added end_date
    Added `end_date` for the events with span of several dates. `event_date` will be used as the start date as there is no need to maintain redundant information

--- db/update.sql
+++ db/update.sql
@@ -35,6 +35,7 @@ CREATE TABLE IF NOT EXISTS `Events` (
   `description` text NOT NULL,
   `organizer` varchar(255) NOT NULL,
   `event_date` int(11) NOT NULL,
+  `end_date` int(11) NOT NULL,
   `event_time` int(11) NOT NULL,
   `venue` varchar(255) NOT NULL,
   PRIMARY KEY (`event_id`),
