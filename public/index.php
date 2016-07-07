c8831d9aa4af2e0e84f4e80df3ca7bb730688d01
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-07-07 09:26:50 +0530

    Added admin_export to view

--- public/index.php
+++ public/index.php
@@ -136,6 +136,9 @@ if (isset($_REQUEST['p']) && preg_match("/^[a-z0-9_]*$/i", $_REQUEST['p']) && (i
   } elseif ($p == "admin_settings") {
     $title = admin_settings_title();
     $content = admin_settings();
+  } elseif ($p == "admin_export") {
+    $title = admin_export_title();
+    $content = admin_export();
   } elseif ($p == "credits") {
     require_once realpath(__DIR__ . '/../includes/pages/guest_credits.php');
     $title = credits_title();
