a3a5f8c3df95fafeb0b0617cee489564e1c74e05
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-07-09 10:07:46 +0530

    Fix: wrong file commit

--- includes/pages/user_settings.php
+++ includes/pages/user_settings.php
@@ -5,9 +5,9 @@ function settings_title() {
 }
 
 function user_settings() {
-  global $enable_tshirt_size, $tshirt_sizes, $themes, $locales, $database;
+  global $enable_tshirt_size, $tshirt_sizes, $themes, $locales, $display_msg;
   global $user;
-
+  
   $msg = "";
   $nick = $user['Nick'];
   $lastname = $user['Name'];
@@ -26,18 +26,16 @@ function user_settings() {
   $selected_language = $user['Sprache'];
   $planned_arrival_date = $user['planned_arrival_date'];
   $planned_departure_date = $user['planned_departure_date'];
-  $organization = $user['organization'];
-  $organization_web = $user['organization_web'];
-  $facebook = $user['facebook'];
-  $twitter = $user['twitter'];
-  $github = $user['github'];
-  $current_city = $user['current_city'];
+  $timezone = $user['timezone'];
+  $timezone_identifiers = DateTimeZone::listIdentifiers();
+  $message_source = sql_select("SELECT * FROM `Welcome_Message`");
+  $display_message = $message_source[0]['display_msg'];
   $native_lang = $user['native_lang'];
   $other_langs = $user['other_langs'];
-
+  
   if (isset($_REQUEST['submit'])) {
     $ok = true;
-
+    
     if (isset($_REQUEST['mail']) && strlen(strip_request_item('mail')) > 0) {
       $mail = strip_request_item('mail');
       if (! check_email($mail)) {
@@ -48,9 +46,9 @@ function user_settings() {
       $ok = false;
       $msg .= error(_("Please enter your e-mail."), true);
     }
-
+    
     $email_shiftinfo = isset($_REQUEST['email_shiftinfo']);
-
+    
     if (isset($_REQUEST['jabber']) && strlen(strip_request_item('jabber')) > 0) {
       $jabber = strip_request_item('jabber');
       if (! check_email($jabber)) {
@@ -58,28 +56,20 @@ function user_settings() {
         $msg .= error(_("Please check your jabber account information."), true);
       }
     }
-
-    if (isset($_REQUEST['current_city']) && strlen(strip_request_item('current_city')) > 0){
-      $current_city = strip_request_item('current_city');
-      }
-    else {
-      $ok = false;
-      $msg .= error(_("Please enter your Current City."), true);
-    }
-
+    
     if (isset($_REQUEST['tshirt_size']) && isset($tshirt_sizes[$_REQUEST['tshirt_size']]))
       $tshirt_size = $_REQUEST['tshirt_size'];
     elseif ($enable_tshirt_size) {
       $ok = false;
     }
-
+    
     if (isset($_REQUEST['planned_arrival_date']) && DateTime::createFromFormat("Y-m-d", trim($_REQUEST['planned_arrival_date']))) {
       $planned_arrival_date = DateTime::createFromFormat("Y-m-d", trim($_REQUEST['planned_arrival_date']))->getTimestamp();
     } else {
       $ok = false;
       $msg .= error(_("Please enter your planned date of arrival."), true);
     }
-
+    
     if (isset($_REQUEST['planned_departure_date']) && $_REQUEST['planned_departure_date'] != '') {
       if (DateTime::createFromFormat("Y-m-d", trim($_REQUEST['planned_departure_date']))) {
         $planned_departure_date = DateTime::createFromFormat("Y-m-d", trim($_REQUEST['planned_departure_date']))->getTimestamp();
@@ -89,7 +79,6 @@ function user_settings() {
       }
     } else
       $planned_departure_date = null;
-
       // Trivia
     if (isset($_REQUEST['lastname']))
       $lastname = strip_request_item('lastname');
@@ -105,9 +94,9 @@ function user_settings() {
       $mobile = strip_request_item('mobile');
     if (isset($_REQUEST['hometown']))
       $hometown = strip_request_item('hometown');
-
-
-
+    if (isset($_REQUEST['timezone']))
+      $timezone = strip_request_item('timezone');
+      
     if ($ok) {
       sql_query("
           UPDATE `User` SET
@@ -123,16 +112,15 @@ function user_settings() {
           `jabber`='" . sql_escape($jabber) . "',
           `Size`='" . sql_escape($tshirt_size) . "',
           `Hometown`='" . sql_escape($hometown) . "',
-          `current_city`='". sql_escape($current_city) . "',
           `planned_arrival_date`='" . sql_escape($planned_arrival_date) . "',
           `planned_departure_date`=" . sql_null($planned_departure_date) . "
+          `timezone`='" . sql_escape($timezone) . "',
           WHERE `UID`='" . sql_escape($user['UID']) . "'");
-
+      
       success(_("Settings saved."));
       redirect(page_link_to('user_settings'));
     }
-  }
-  elseif (isset($_REQUEST['submit_sn'])) {
+  } elseif (isset($_REQUEST['submit_sn'])) {
       $ok = true;
 
       if (isset($_REQUEST['twitter']))
@@ -201,10 +189,10 @@ function user_settings() {
         success(_("Languages Updates saved."));
       redirect(page_link_to('user_settings'));
     }
-  }
+  } 
   elseif (isset($_REQUEST['submit_password'])) {
     $ok = true;
-
+    
     if (! isset($_REQUEST['password']) || ! verify_password($_REQUEST['password'], $user['Passwort'], $user['UID']))
       $msg .= error(_("-> not OK. Please try again."), true);
     elseif (strlen($_REQUEST['new_password']) < MIN_PASSWORD_LENGTH)
@@ -218,66 +206,56 @@ function user_settings() {
     redirect(page_link_to('user_settings'));
   } elseif (isset($_REQUEST['submit_theme'])) {
     $ok = true;
-
+    
     if (isset($_REQUEST['theme']) && isset($themes[$_REQUEST['theme']]))
       $selected_theme = $_REQUEST['theme'];
     else
       $ok = false;
-
+    
     if ($ok) {
       sql_query("UPDATE `User` SET `color`='" . sql_escape($selected_theme) . "' WHERE `UID`='" . sql_escape($user['UID']) . "'");
-
+      
       success(_("Theme changed."));
       redirect(page_link_to('user_settings'));
     }
   } elseif (isset($_REQUEST['submit_language'])) {
     $ok = true;
-
+    
     if (isset($_REQUEST['language']) && isset($locales[$_REQUEST['language']]))
       $selected_language = $_REQUEST['language'];
     else
       $ok = false;
-
+    
     if ($ok) {
       sql_query("UPDATE `User` SET `Sprache`='" . sql_escape($selected_language) . "' WHERE `UID`='" . sql_escape($user['UID']) . "'");
       $_SESSION['locale'] = $selected_language;
-
+      
       success("Language changed.");
       redirect(page_link_to('user_settings'));
     }
+  }elseif (isset($_REQUEST['submit_message'])){
+      $ok=true;
+
+      if(isset($_REQUEST['display_message']))
+        $display_message=strip_request_item('display_message');
+      else
+        $ok = false;
+      
+      if($ok){
+        sql_query("UPDATE `Welcome_Message` SET `display_msg`='" . sql_escape($display_message) . "'");
+        
+        success("Message Changed");
+        redirect(page_link_to('user_settings'));
+      }
   }
-  elseif (isset($_REQUEST['submit_database'])) {
-    $ok = true;
-
-    if (isset($_REQUEST['dbase']) && isset($database[$_REQUEST['dbase']])){
-
-      $user = 'stanfori_dk';
-      $pass = 'dishantk1807#';
-      $server = 'localhost';
-
-$dbh = new PDO( "mysql:host=$server", $user, $pass );
-$database = $dbh->query( 'SHOW DATABASES' );
-}
-
-    else
-      $ok = false;
-
-
-  }
-  elseif (isset($_REQUEST['reinstall'])) {
-    $ok = true;
-
-      success("System Re-insatlled.");
-      redirect(page_link_to('user_settings'));
-    }
-
+ 
   if ($ok) {
       $_SESSION['uid'] = $login_user['UID'];
       $_SESSION['locale'] = $login_user['Sprache'];
-
-
-    }
-if( $_SESSION['uid'] == 1){
+  }
+ 
+ // Admin settings page   
+if( $_SESSION['uid'] == 1){  
   return page_with_title("Admin Settings", array(
       $msg,
       msg(),
@@ -300,48 +278,41 @@ if( $_SESSION['uid'] == 1){
                   form_text('jabber', _("Jabber"), $jabber),
                   form_text('hometown', _("Hometown"), $hometown),
                   $enable_tshirt_size ? form_select('tshirt_size', _("Shirt size"), $tshirt_sizes, $tshirt_size) : '',
+                  form_select('timezone', _("Timezone") . ' ' . entry_required(), $timezone_identifiers , $timezone),          
                   form_info('', _('Please visit the angeltypes page to manage your angeltypes.')),
-                  form_submit('submit', _("Save"))
-              ))
+                  form_submit('submit', _("Save")) 
+              )) 
           )),
-
           div('col-md-6', array(
               form(array(
                   form_info(_("Here you can change your password.")),
                   form_password('password', _("Old password:")),
                   form_password('new_password', _("New password:")),
                   form_password('new_password2', _("Password confirmation:")),
-                  form_submit('submit_password', _("Save"))
+                  form_submit('submit_password', _("Save")) 
               )),
-
               form(array(
                   form_info(_("Here you can choose your color settings:")),
                   form_select('theme', _("Color settings:"), $themes, $selected_theme),
-                  form_submit('submit_theme', _("Save"))
+                  form_submit('submit_theme', _("Save")) 
               )),
               form(array(
                   form_info(_("Here you can choose your language:")),
                   form_select('language', _("Language:"), $locales, $selected_language),
-                  form_submit('submit_language', _("Save"))
-              )),
-
-             form(array(
-              	  form_info(_("Here you can choose your Database:")),
-              	  form_select('dbase',_("Databases:"), $database, $selected_database),
-              	  form_submit('submit_database', _("Save"))
+                  form_submit('submit_language', _("Save")) 
               )),
               form(array(
-                  form_info(_("Here you can reinstall the system")),
-                  form_submit('reinstall', _("Reinstall"))
-              ))
-
+                  form_info(_("Here you can write your display message for registration:")),
+                  form_text('display_message', _("Message"), $display_message),
+                  form_submit('submit_message', _("Save"))
+              )) 
           ))
-
-      ))
+      )) 
   ));
 }
 
-if( $_SESSION['uid'] > 1){
+// User settings page
+if( $_SESSION['uid'] > 1){  
   return page_with_title("User Settings", array(
       $msg,
       msg(),
@@ -365,18 +336,18 @@ if( $_SESSION['uid'] > 1){
                   form_text('hometown', _("Hometown"), $hometown),
                   form_text('current_city', _("Current City") . ' ' . entry_required(), $current_city),
                   $enable_tshirt_size ? form_select('tshirt_size', _("Shirt size"), $tshirt_sizes, $tshirt_size) : '',
+                  form_select('timezone', _("Timezone") . ' ' . entry_required(), $timezone_identifiers , $timezone),                  
                   form_info('', _('Please visit the angeltypes page to manage your angeltypes.')),
-                  form_submit('submit', _("Save"))
-              ))
+                  form_submit('submit', _("Save")) 
+              )) 
           )),
-
           div('col-md-6', array(
               form(array(
                   form_info(_("Here you can change your password.")),
                   form_password('password', _("Old password:")),
                   form_password('new_password', _("New password:")),
                   form_password('new_password2', _("Password confirmation:")),
-                  form_submit('submit_password', _("Save"))
+                  form_submit('submit_password', _("Save")) 
               )),
               form(array(
                   form_info(_("Here you can change links to your social networks")),
@@ -400,17 +371,15 @@ if( $_SESSION['uid'] > 1){
               form(array(
                   form_info(_("Here you can choose your color settings:")),
                   form_select('theme', _("Color settings:"), $themes, $selected_theme),
-                  form_submit('submit_theme', _("Save"))
+                  form_submit('submit_theme', _("Save")) 
               )),
               form(array(
                   form_info(_("Here you can choose your language:")),
                   form_select('language', _("Language:"), $locales, $selected_language),
-                  form_submit('submit_language', _("Save"))
+                  form_submit('submit_language', _("Save")) 
               ))
-
           ))
-
-      ))
+      )) 
   ));
 }
 }
