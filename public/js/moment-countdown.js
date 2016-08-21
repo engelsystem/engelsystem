e80e2e9c2b4a77eae19864af8d5b30bbf9a25fd5
msquare <msquare@notrademark.de>
2016-08-21 21:21:13 +0200

    add check for undefined variables in moment-countdown.js and add comments

--- public/js/moment-countdown.js
+++ public/js/moment-countdown.js
@@ -1,11 +1,18 @@
+/**
+ * Initialize all moment countdowns on the page. A moment countdown has the
+ * class "moment-countdown" and the attribute "data-timestamp" which defines the
+ * countdown's goal.
+ */
 $(document).ready(function() {
+  if (typeof moment !== "undefined") {
     $.each($('.moment-countdown'), function(i, e) {
-        var span = $(e);
-        var text = span.html();
-        var timestamp = moment(parseInt(span.attr("data-timestamp") * 1000));
+      var span = $(e);
+      var text = span.html();
+      var timestamp = moment(parseInt(span.attr("data-timestamp") * 1000));
+      span.html(text.replace("%c", timestamp.fromNow()));
+      setInterval(function() {
         span.html(text.replace("%c", timestamp.fromNow()));
-        setInterval(function() {
-            span.html(text.replace("%c", timestamp.fromNow()));
-        }, 1000);
+      }, 1000);
     });
+  }
 });
