deac80e700d69a56992fec70997a9d54716bdafa
msquare <msquare@notrademark.de>
2016-12-13 17:58:41 +0100

    merge #253

--- db/update.sql
+++ db/update.sql
@@ -7,3 +7,12 @@ ALTER TABLE `User` ADD COLUMN `email_by_human_allowed` BOOLEAN NOT NULL;
 
 -- No Self Sign Up for some Angel Types
 ALTER TABLE engelsystem.AngelTypes ADD no_self_signup TINYINT(1) NOT NULL;
+
+ALTER TABLE `AngelTypes`  
+  ADD `contact_user_id` INT NULL,  
+  ADD `contact_name` VARCHAR(250) NULL,  
+  ADD `contact_dect` VARCHAR(5) NULL,  
+  ADD `contact_email` VARCHAR(250) NULL,  
+  ADD INDEX  (`contact_user_id`);
+ALTER TABLE `AngelTypes` 
+  ADD  FOREIGN KEY (`contact_user_id`) REFERENCES `User`(`UID`) ON DELETE SET NULL ON UPDATE CASCADE;
