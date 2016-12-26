b3f92834b0d531d479b7d2f99dbe04fa30faf3f0
msquare <msquare@notrademark.de>
2016-12-26 14:33:18 +0100

    reduce number of sql queries on shift calendar

--- includes/model/NeededAngelTypes_model.php
+++ includes/model/NeededAngelTypes_model.php
@@ -87,14 +87,15 @@ function NeededAngelTypes_by_shift($shiftId) {
   $shift_entries = ShiftEntries_by_shift($shiftId);
   $needed_angeltypes = [];
   foreach ($needed_angeltypes_source as $angeltype) {
+    $angeltype['shift_entries'] = [];
     $angeltype['taken'] = 0;
-    foreach($shift_entries as $shift_entry) {
-      if($shift_entry['TID']==$angeltype['id'] && $shift_entry['freeloaded'] == 0) {
-        $angeltype['taken']++;
+    foreach ($shift_entries as $shift_entry) {
+      if ($shift_entry['TID'] == $angeltype['angel_type_id'] && $shift_entry['freeloaded'] == 0) {
+        $angeltype['taken'] ++;
+        $angeltype['shift_entries'][] = $shift_entry;
       }
     }
     
-    $angeltype['shift_entries'] = $shift_entries;
     $needed_angeltypes[] = $angeltype;
   }
   
