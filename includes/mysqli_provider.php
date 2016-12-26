549cd6534aea23affd530c45449dfdbd08b55921
msquare <msquare@notrademark.de>
2016-12-26 14:40:28 +0100

    reduce number of sql queries on shift calendar

--- includes/mysqli_provider.php
+++ includes/mysqli_provider.php
@@ -129,10 +129,7 @@ function sql_select_db($db_name) {
  * @return Result array or false on error
  */
 function sql_select($query) {
-  global $sql_connection;
-  
-//   echo $query . ";\n";
-//   echo debug_string_backtrace() . "\n";
+  global $sql_connection, $query_count;
   
   $result = $sql_connection->query($query);
   if ($result) {
