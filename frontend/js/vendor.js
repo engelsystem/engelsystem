ccd061d3cb2136b195a4f33adc1391c0042415ef
marudor <marudor@marudor.de>
2018-08-03 11:35:44 +0200

    Correctly expose jquery

--- frontend/js/vendor.js
+++ frontend/js/vendor.js
@@ -1,18 +1,16 @@
-import 'imports-loader?module=>false!jquery';
-import 'imports-loader?define=>false!jquery-ui';
-import 'bootstrap';
-import 'imports-loader?define=>false&exports=>false!bootstrap-datepicker';
-import 'bootstrap-datepicker/js/locales/bootstrap-datepicker.de';
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css';
-import 'imports-loader?this=>window!chart.js';
-import 'imports-loader?this=>window&define=>false&exports=>false!moment';
-import 'imports-loader?this=>window&define=>false&exports=>false!moment/locale/de';
-import './forms';
-import './sticky-headers';
-import './moment-countdown';
+window.$ = window.jQuery = require('jquery');
+require('imports-loader?define=>false!jquery-ui');
+require('bootstrap');
+require('imports-loader?define=>false&exports=>false!bootstrap-datepicker');
+require('bootstrap-datepicker/js/locales/bootstrap-datepicker.de');
+require('bootstrap-datepicker/dist/css/bootstrap-datepicker3.min.css');
+require('imports-loader?this=>window!chart.js');
+require('imports-loader?this=>window&define=>false&exports=>false!moment');
+require('imports-loader?this=>window&define=>false&exports=>false!moment/locale/de');
+require('./forms');
+require('./sticky-headers');
+require('./moment-countdown');
 
 $(function () {
     moment.locale("%locale%");
 });
-
-
