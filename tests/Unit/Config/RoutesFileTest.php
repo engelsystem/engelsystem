64c4743f57c2444a2e40ab4f115cbc6bdc9bcde1
Igor Scheller <igor.scheller@igorshp.de>
2019-09-06 17:30:04 +0200

    Routes config: Added test to ensure that routes are cacheable in production

---
+++ tests/Unit/Config/RoutesFileTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Engelsystem\Test\Unit\Config;
+
+use FastRoute\RouteCollector;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class RoutesFileTest extends TestCase
+{
+    /**
+     * @doesNotPerformAssertions
+     */
+    public function testLoadRoutes()
+    {
+        /** @var RouteCollector|MockObject $route */
+        $route = $this->getMockBuilder(RouteCollector::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['addRoute'])
+            ->getMock();
+
+        $this->doesNotPerformAssertions();
+        /** @see RouteCollector::addRoute */
+        $route->expects($this->any())
+            ->method('addRoute')
+            ->willReturnCallback(function ($httpMethod, $route, $handler) {
+                /**
+                 * @param string|string[] $httpMethod
+                 * @param string          $route
+                 * @param mixed           $handler
+                 */
+                if (is_string($handler) || (is_array($handler) && is_string($handler[0]))) {
+                    return;
+                }
+
+                $this->fail(
+                    sprintf('The route "%s %s" is not cacheable', implode(',', (array)$httpMethod), $route)
+                );
+            });
+
+        require __DIR__ . '/../../../config/routes.php';
+    }
+}
