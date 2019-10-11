75f04507aedc6cea020da93fc579351cfb0f4f2c
Michael Weimann <mail@michael-weimann.eu>
2019-10-11 21:16:24 +0200

    Makes Monday first day of the week even if English language is selected

--- resources/assets/js/vendor.js
+++ resources/assets/js/vendor.js
@@ -10,6 +10,13 @@ require('./forms');
 require('./sticky-headers');
 require('./moment-countdown');
 
+moment.updateLocale('en', {
+    week : {
+        dow : 1, // Monday is the first day of the week.
+        doy : 4  // The week that contains Jan 4th is the first week of the year.
+    }
+});
+
 $.ajaxSetup({
     headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')}
 });
