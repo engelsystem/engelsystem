f6a7cb9f76b02c9bffdbb5c2a8063efbdc7d3a64
yourcoke <deinkoks@gmail.com>
2019-11-25 22:33:13 +0100

    Grouped all api endpoints with addGroup(), added api version name.

--- config/routes.php
+++ config/routes.php
@@ -24,9 +24,11 @@ $route->get('/metrics', 'Metrics\\Controller@metrics');
 $route->get('/stats', 'Metrics\\Controller@stats');
 
 // API
-$route->get('/api/angeltypes/my', 'ApiController@getMyAngelTypes');
-$route->get('/api/angeltypes', 'ApiController@getAngelTypes');
-$route->get('/api/shifts/my', 'ApiController@getMyShifts');
-$route->get('/api/shifts/free/{start:.+}/until/{stop:.+}', 'ApiController@getShiftsFree');
-$route->get('/api/shifts/by/angeltype/{angeltypeid:.+}', 'ApiController@getShiftsByAngelType');
-$route->get('/api[/{resource:.+}]', 'ApiController@index');
+$route->addGroup('/api/v2019-alpha', function (RouteCollector $route) {
+    $route->get('/angeltypes/my', 'ApiController@getMyAngelTypes');
+    $route->get('/angeltypes', 'ApiController@getAngelTypes');
+    $route->get('/shifts/my', 'ApiController@getMyShifts');
+    $route->get('/shifts/free/{start:.+}/until/{stop:.+}', 'ApiController@getShiftsFree');
+    $route->get('/shifts/by/angeltype/{angeltypeid:.+}', 'ApiController@getShiftsByAngelType');
+    $route->get('[/{resource:.+}]', 'ApiController@index');
+});
