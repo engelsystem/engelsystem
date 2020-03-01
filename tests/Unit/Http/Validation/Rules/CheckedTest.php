e42284deca807915e2d9c5a042fba4377d453ad8
Igor Scheller <igor.scheller@igorshp.de>
2020-03-01 02:39:06 +0100

    Added Checked validation

---
+++ tests/Unit/Http/Validation/Rules/CheckedTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Engelsystem\Test\Unit\Http\Validation\Rules;
+
+use Engelsystem\Http\Validation\Rules\Checked;
+use Engelsystem\Test\Unit\TestCase;
+
+class CheckedTest extends TestCase
+{
+    /**
+     * @covers \Engelsystem\Http\Validation\Rules\Checked::validate
+     */
+    public function testValidate()
+    {
+        $rule = new Checked();
+
+        $this->assertTrue($rule->validate('yes'));
+        $this->assertTrue($rule->validate('on'));
+        $this->assertTrue($rule->validate(1));
+        $this->assertTrue($rule->validate('1'));
+        $this->assertTrue($rule->validate('true'));
+        $this->assertTrue($rule->validate(true));
+
+        $this->assertFalse($rule->validate('no'));
+        $this->assertFalse($rule->validate('off'));
+        $this->assertFalse($rule->validate(0));
+        $this->assertFalse($rule->validate('0'));
+        $this->assertFalse($rule->validate('false'));
+        $this->assertFalse($rule->validate(false));
+        $this->assertFalse($rule->validate(null));
+    }
+}
