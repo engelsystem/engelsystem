e5be440f06a22b4abedf2352ad16c6253cffe3de
Igor Scheller <igor.scheller@igorshp.de>
2020-04-19 19:59:23 +0200

    Composer: Be 2.0 compatible by fixing namespaces

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,8 +1,3 @@
 <?php
 
-use Composer\Autoload\ClassLoader;
-
 require_once __DIR__ . '/../includes/autoload.php';
-
-/** @var ClassLoader $loader */
-$loader->addPsr4('Engelsystem\\Test\\', __DIR__ . '/');
