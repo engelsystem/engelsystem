6444f9e4b22dd778440762f1551f038e03a4d061
Igor Scheller <igor.scheller@igorshp.de>
2020-04-20 00:01:37 +0200

    Check permissions before showing ical, atom and json export links
    closes #729 (Rechtesystem ical&json)

--- src/Middleware/SessionHandlerServiceProvider.php
+++ src/Middleware/SessionHandlerServiceProvider.php
@@ -14,6 +14,7 @@ class SessionHandlerServiceProvider extends ServiceProvider
             ->give(function () {
                 return [
                     '/api',
+                    '/atom',
                     '/ical',
                     '/metrics',
                     '/shifts-json-export',
