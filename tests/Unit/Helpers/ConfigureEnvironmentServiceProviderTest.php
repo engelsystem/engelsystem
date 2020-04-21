2605204af8533fb4da1c188b1ca94d62bf2f50fb
Igor Scheller <igor.scheller@igorshp.de>
2020-04-21 12:19:31 +0200

    Moved error reporting settings to ConfigureEnvironmentServiceProvider

--- tests/Unit/Helpers/ConfigureEnvironmentServiceProviderTest.php
+++ tests/Unit/Helpers/ConfigureEnvironmentServiceProviderTest.php
@@ -15,21 +15,29 @@ class ConfigureEnvironmentServiceProviderTest extends ServiceProviderTest
      */
     public function testRegister()
     {
-        $config = new Config(['timezone' => 'Australia/Eucla']);
+        $config = new Config(['timezone' => 'Australia/Eucla', 'environment' => 'production']);
         $this->app->instance('config', $config);
 
         /** @var ConfigureEnvironmentServiceProvider|MockObject $serviceProvider */
         $serviceProvider = $this->getMockBuilder(ConfigureEnvironmentServiceProvider::class)
             ->setConstructorArgs([$this->app])
-            ->onlyMethods(['setTimeZone'])
+            ->onlyMethods(['setTimeZone', 'displayErrors', 'errorReporting'])
             ->getMock();
 
-        $serviceProvider->expects($this->once())
+        $serviceProvider->expects($this->exactly(2))
             ->method('setTimeZone')
             ->willReturnCallback(function (CarbonTimeZone $timeZone) {
                 $this->assertEquals('Australia/Eucla', $timeZone->getName());
             });
+        $serviceProvider->expects($this->exactly(3))
+            ->method('displayErrors')
+            ->withConsecutive([false], [false], [true]);
+        $serviceProvider->expects($this->exactly(1))
+            ->method('errorReporting')
+            ->with(E_ALL);
 
+        $serviceProvider->register();
+        $config->set('environment', 'development');
         $serviceProvider->register();
     }
 }
