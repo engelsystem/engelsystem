d0e3abc328b4c3f5daf70c98560e5d17b5168248
Igor Scheller <igor.scheller@igorshp.de>
2020-04-21 12:39:54 +0200

    Moved development error handler setup to ConfigureEnvironmentServiceProvider

--- includes/application.php
+++ includes/application.php
@@ -2,15 +2,11 @@
 
 use Engelsystem\Application;
 use Engelsystem\Config\Config;
-use Engelsystem\Exceptions\Handler;
-use Engelsystem\Exceptions\Handlers\HandlerInterface;
-
 
 /**
  * Include the autoloader
  */
 require_once __DIR__ . '/autoload.php';
-
 
 /**
  * Initialize and bootstrap the application
@@ -19,13 +15,3 @@ $app = new Application(realpath(__DIR__ . DIRECTORY_SEPARATOR . '..'));
 $appConfig = $app->make(Config::class);
 $appConfig->set(require config_path('app.php'));
 $app->bootstrap($appConfig);
-
-
-/**
- * Configure environment
- */
-if (config('environment') == 'development') {
-    $errorHandler = $app->get('error.handler');
-    $errorHandler->setEnvironment(Handler::ENV_DEVELOPMENT);
-    $app->bind(HandlerInterface::class, 'error.handler.development');
-}
