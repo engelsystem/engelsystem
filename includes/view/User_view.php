b5e0a46dbd19ccd5cbe777f548408c9522aa40b7
Igor Scheller <igor.scheller@igorshp.de>
2020-10-18 01:07:23 +0200

    My Shifts: Link Angeltypes
    Closes #707 (The angel types in the "My Shifts" view should be linked to their description page)

--- includes/view/User_view.php
+++ includes/view/User_view.php
@@ -339,7 +339,9 @@ function User_last_shift_render($user)
  */
 function User_view_shiftentries($needed_angel_type)
 {
-    $shift_info = '<br><b>' . $needed_angel_type['name'] . ':</b> ';
+    $shift_info = '<br><a href="'
+        . page_link_to('angeltypes', ['action' => 'view', 'angeltype_id' => $needed_angel_type['id']])
+        .'"><b>' . $needed_angel_type['name'] . '</a>:</b> ';
 
     $shift_entries = [];
     foreach ($needed_angel_type['users'] as $user_shift) {
