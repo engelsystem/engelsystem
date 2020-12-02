5cdf3889f95e8158128c0993a3b157fed43eb175
Igor Scheller <igor.scheller@igorshp.de>
2020-12-02 14:43:11 +0100

    Add max length for user settings

--- includes/model/User_model.php
+++ includes/model/User_model.php
@@ -112,7 +112,7 @@ function User_validate_Nick($nick)
 {
     $nick = trim($nick);
 
-    if (strlen($nick) == 0 || strlen($nick) > 23) {
+    if (strlen($nick) == 0 || strlen($nick) > 24) {
         return new ValidationResult(false, $nick);
     }
     if (preg_match('/([^\p{L}\p{N}\-_. ]+)/ui', $nick)) {
