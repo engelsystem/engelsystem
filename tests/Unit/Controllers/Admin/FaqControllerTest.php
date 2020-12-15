fbd1241fc1fb3ae4612ccdf1f8c92da71fc919fb
Igor Scheller <igor.scheller@igorshp.de>
2020-12-15 13:39:00 +0100

    Tests: Added abstract ControllerTest

--- tests/Unit/Controllers/Admin/FaqControllerTest.php
+++ tests/Unit/Controllers/Admin/FaqControllerTest.php
@@ -2,43 +2,21 @@
 
 namespace Engelsystem\Test\Unit\Controllers\Admin;
 
-use Engelsystem\Config\Config;
 use Engelsystem\Controllers\Admin\FaqController;
-use Engelsystem\Helpers\Authenticator;
 use Engelsystem\Http\Exceptions\ValidationException;
-use Engelsystem\Http\Request;
-use Engelsystem\Http\Response;
-use Engelsystem\Http\UrlGenerator;
-use Engelsystem\Http\UrlGeneratorInterface;
 use Engelsystem\Http\Validation\Validator;
 use Engelsystem\Models\Faq;
-use Engelsystem\Test\Unit\HasDatabase;
-use Engelsystem\Test\Unit\TestCase;
+use Engelsystem\Test\Unit\Controllers\ControllerTest;
 use Illuminate\Support\Collection;
-use PHPUnit\Framework\MockObject\MockObject;
-use Psr\Log\LoggerInterface;
-use Psr\Log\Test\TestLogger;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
 
-class FaqControllerTest extends TestCase
+class FaqControllerTest extends ControllerTest
 {
-    use HasDatabase;
-
     /** @var array */
     protected $data = [
         'question' => 'Foo?',
         'text'     => 'Bar!',
     ];
-
-    /** @var TestLogger */
-    protected $log;
-
-    /** @var Response|MockObject */
-    protected $response;
-
-    /** @var Request */
-    protected $request;
 
     /**
      * @covers \Engelsystem\Controllers\Admin\FaqController::__construct
@@ -185,22 +163,6 @@ class FaqControllerTest extends TestCase
     public function setUp(): void
     {
         parent::setUp();
-        $this->initDatabase();
-
-        $this->request = Request::create('http://localhost');
-        $this->app->instance('request', $this->request);
-
-        $this->response = $this->createMock(Response::class);
-        $this->app->instance(Response::class, $this->response);
-
-        $this->log = new TestLogger();
-        $this->app->instance(LoggerInterface::class, $this->log);
-
-        $this->app->instance('session', new Session(new MockArraySessionStorage()));
-
-        $this->app->bind(UrlGeneratorInterface::class, UrlGenerator::class);
-
-        $this->app->instance('config', new Config());
 
         (new Faq([
             'question' => 'Lorem',
