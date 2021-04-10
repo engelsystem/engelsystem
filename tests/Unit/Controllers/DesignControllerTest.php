a5757497e5f83fa6e17b43b92aa09ad9a316bdbf
Igor Scheller <igor.scheller@igorshp.de>
2021-04-10 17:02:34 +0200

    Add theme change to /design page

--- tests/Unit/Controllers/DesignControllerTest.php
+++ tests/Unit/Controllers/DesignControllerTest.php
@@ -2,7 +2,9 @@
 
 namespace Engelsystem\Test\Unit\Controllers;
 
+use Engelsystem\Config\Config;
 use Engelsystem\Controllers\DesignController;
+use Engelsystem\Http\Request;
 use Engelsystem\Http\Response;
 use Engelsystem\Test\Unit\TestCase;
 use PHPUnit\Framework\MockObject\MockObject;
@@ -21,10 +23,35 @@ class DesignControllerTest extends TestCase
             ->method('withView')
             ->with('pages/design')
             ->willReturn($response);
+        $request = new Request(['theme' => 42]);
+        $config = new Config();
 
-        $controller = new DesignController($response);
-        $return = $controller->index();
+        $controller = new DesignController($response, $config);
+        $return = $controller->index($request);
 
         $this->assertEquals($response, $return);
+    }
+
+    /**
+     * @covers \Engelsystem\Controllers\DesignController::index
+     */
+    public function testIndexSetTheme()
+    {
+        /** @var Response|MockObject $response */
+        $response = $this->createMock(Response::class);
+        $response->expects($this->once())
+            ->method('withView')
+            ->willReturnCallback(function (string $view, array $data) use ($response) {
+                $this->assertTrue(isset($data['theme']));
+                $this->assertEquals('42', $data['theme']);
+
+                return $response;
+            });
+        $request = new Request();
+        $request->attributes->set('theme', '42');
+        $config = new Config(['available_themes' => ['42' => 'Meaning of Live']]);
+
+        $controller = new DesignController($response, $config);
+        $controller->index($request);
     }
 }
