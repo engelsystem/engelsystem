6c288bceb51306e4366675408b255049fa7c110f
Igor Scheller <igor.scheller@igorshp.de>
2021-12-03 23:27:11 +0100

    Fixed maintenance page assets for subpaths and send 503 Service Unavailable

--- includes/engelsystem.php
+++ includes/engelsystem.php
@@ -3,6 +3,9 @@
 /**
  * Bootstrap application
  */
+
+use Engelsystem\Http\UrlGeneratorInterface;
+
 require __DIR__ . '/application.php';
 
 
@@ -16,8 +19,11 @@ require __DIR__ . '/includes.php';
  * Check for maintenance
  */
 if ($app->get('config')->get('maintenance')) {
+    http_response_code(503);
+    $url = $app->get(UrlGeneratorInterface::class);
     $maintenance = file_get_contents(__DIR__ . '/../resources/views/layouts/maintenance.html');
     $maintenance = str_replace('%APP_NAME%', $app->get('config')->get('app_name'), $maintenance);
+    $maintenance = str_replace('%ASSETS_PATH%', $url->to(''), $maintenance);
     echo $maintenance;
     die();
 }
