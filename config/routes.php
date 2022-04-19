bad9c1385e3e1b4749f8b64980c15ad9cb2e2978
frischler <frischler@posteo.de>
2022-04-19 20:23:47 +0200

    Added new conversation page. Added delete button.

--- config/routes.php
+++ config/routes.php
@@ -52,8 +52,11 @@ $route->post('/questions/new', 'QuestionsController@save');
 
 // Messages
 $route->get('/messages', 'MessagesController@index');
-$route->get('/messages/{id:\d+}', 'MessagesController@conversation');
-$route->post('/messages/{id:\d+}', 'MessagesController@send');
+$route->get('/messages/new_conversation', 'MessagesController@newConversation');
+$route->post('/messages/new_conversation', 'MessagesController@send');
+$route->get('/messages/{user_id:\d+}', 'MessagesController@conversation');
+$route->post('/messages/{user_id:\d+}', 'MessagesController@send');
+$route->post('/messages/{user_id:\d+}/{msg_id:\d+}', 'MessagesController@delete');
 
 // API
 $route->get('/api[/{resource:.+}]', 'ApiController@index');
