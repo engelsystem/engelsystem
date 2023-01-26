b382af50e5f7f5ed2f5553325e058f7a979bd153
Thomas Rupprecht <rupprecht.thomas@gmail.com>
2023-01-26 22:04:55 +0100

    simplify check in js ready function

--- resources/assets/js/ready.js
+++ resources/assets/js/ready.js
@@ -2,9 +2,9 @@
  * @param {Function} callback
  */
 export const ready = (callback) => {
-  if (document.readyState !== 'loading') {
-    callback();
-  } else {
+  if (document.readyState === 'loading') {
     document.addEventListener('DOMContentLoaded', callback);
+  } else {
+    callback();
   }
 };
