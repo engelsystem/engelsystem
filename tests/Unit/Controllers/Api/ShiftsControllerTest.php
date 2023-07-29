ef3bd7c319884974a2fca0461d0cf625b586de52
Igor Scheller <igor.scheller@igorshp.de>
2023-07-29 21:37:46 +0200

    API: Show needed/added users by angeltype in shifts

--- tests/Unit/Controllers/Api/ShiftsControllerTest.php
+++ tests/Unit/Controllers/Api/ShiftsControllerTest.php
@@ -8,16 +8,19 @@ use Engelsystem\Controllers\Api\ShiftsController;
 use Engelsystem\Http\Request;
 use Engelsystem\Http\Response;
 use Engelsystem\Models\Room;
+use Engelsystem\Models\Shifts\NeededAngelType;
 use Engelsystem\Models\Shifts\Shift;
 use Engelsystem\Models\Shifts\ShiftEntry;
 use Engelsystem\Models\User\Contact;
 use Engelsystem\Models\User\PersonalData;
 use Engelsystem\Models\User\User;
+use Illuminate\Database\Eloquent\Collection;
 
 class ShiftsControllerTest extends ApiBaseControllerTest
 {
     /**
      * @covers \Engelsystem\Controllers\Api\ShiftsController::entriesByRoom
+     * @covers \Engelsystem\Controllers\Api\ShiftsController::getNeededAngelTypes
      */
     public function testEntriesByRoom(): void
     {
@@ -26,9 +29,42 @@ class ShiftsControllerTest extends ApiBaseControllerTest
         /** @var Room $room */
         $room = Room::factory()->create();
 
-        Shift::factory(3)
-            ->has(ShiftEntry::factory(2), 'shiftEntries')
+        // Shifts
+        /** @var Collection|Shift[] $shifts */
+        $shifts = Shift::factory(2)
             ->create(['room_id' => $room->id]);
+        $shiftA = $shifts[0];
+
+        // "Empty" entry to be skipped
+        NeededAngelType::factory(1)->create(['room_id' => null, 'shift_id' => $shiftA->id, 'count' => 0]);
+
+        // Needed entry by shift
+        /** @var NeededAngelType $byShift */
+        $byShift = NeededAngelType::factory(2)
+            ->create(['room_id' => null, 'shift_id' => $shiftA->id, 'count' => 2])
+            ->first();
+
+        // Needed entry by room
+        /** @var NeededAngelType $byRoom */
+        $byRoom = NeededAngelType::factory(1)
+            ->create(['room_id' => $room->id, 'shift_id' => null, 'count' => 3])
+            ->first();
+
+        // Added by both
+        NeededAngelType::factory(1)
+            ->create([
+                'room_id' => $room->id, 'shift_id' => null, 'angel_type_id' => $byShift->angel_type_id, 'count' => 3,
+            ])
+            ->first();
+
+        // By shift
+        ShiftEntry::factory(2)->create(['shift_id' => $shiftA->id, 'angel_type_id' => $byShift->angel_type_id]);
+
+        // By room
+        ShiftEntry::factory(1)->create(['shift_id' => $shiftA->id, 'angel_type_id' => $byRoom->angel_type_id]);
+
+        // Additional (not required by shift nor room)
+        ShiftEntry::factory(1)->create(['shift_id' => $shiftA->id]);
 
         foreach (User::all() as $user) {
             // Generate user data
@@ -50,6 +86,6 @@ class ShiftsControllerTest extends ApiBaseControllerTest
 
         $data = json_decode($response->getContent(), true);
         $this->assertArrayHasKey('data', $data);
-        $this->assertCount(3, $data['data']);
+        $this->assertCount(2, $data['data']);
     }
 }
