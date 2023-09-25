fddae626692a33111b49f706d34b077afa79c6a2
Igor Scheller <igor.scheller@igorshp.de>
2023-09-25 17:16:12 +0200

    Tests: Fix string length validation tests

--- tests/Unit/Http/Validation/Rules/MaxTest.php
+++ tests/Unit/Http/Validation/Rules/MaxTest.php
@@ -18,11 +18,16 @@ class MaxTest extends TestCase
         $this->assertFalse($rule->validate(10));
         $this->assertFalse($rule->validate('22'));
         $this->assertTrue($rule->validate(3));
+        $this->assertTrue($rule->validate(2));
+        $this->assertTrue($rule->validate(-10));
         $this->assertFalse($rule->validate('TEST'));
         $this->assertTrue($rule->validate('AS'));
 
         $rule = new Max('2042-01-01');
         $this->assertFalse($rule->validate('2100-01-01'));
+        $this->assertFalse($rule->validate('2042-01-02'));
         $this->assertTrue($rule->validate('2000-01-01'));
+        $this->assertTrue($rule->validate('2041-12-31'));
+        $this->assertTrue($rule->validate('2042-01-01'));
     }
 }
--- tests/Unit/Http/Validation/Rules/MinTest.php
+++ tests/Unit/Http/Validation/Rules/MinTest.php
@@ -15,14 +15,19 @@ class MinTest extends TestCase
     public function testValidate(): void
     {
         $rule = new Min(3);
+        $this->assertFalse($rule->validate(-10));
         $this->assertFalse($rule->validate(1));
         $this->assertFalse($rule->validate('2'));
         $this->assertTrue($rule->validate(3));
+        $this->assertTrue($rule->validate(4));
         $this->assertFalse($rule->validate('AS'));
         $this->assertTrue($rule->validate('TEST'));
 
         $rule = new Min('2042-01-01');
         $this->assertFalse($rule->validate('2000-01-01'));
+        $this->assertFalse($rule->validate('2041-12-31'));
+        $this->assertTrue($rule->validate('2042-01-01'));
+        $this->assertTrue($rule->validate('2042-01-02'));
         $this->assertTrue($rule->validate('2345-01-01'));
     }
 }
