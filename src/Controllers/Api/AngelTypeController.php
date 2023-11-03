15c65d09095a5e4f54148ca33ef11606bb4f59a6
Igor Scheller <igor.scheller@igorshp.de>
2023-11-03 23:27:31 +0100

    API: Add /users/{id}/angeltypes, updated openapi formatting

--- src/Controllers/Api/AngelTypeController.php
+++ src/Controllers/Api/AngelTypeController.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Engelsystem\Controllers\Api;
 
+use Engelsystem\Http\Request;
 use Engelsystem\Http\Response;
 use Engelsystem\Models\AngelType;
+use Engelsystem\Models\User\User;
 
 class AngelTypeController extends ApiController
 {
@@ -16,11 +18,43 @@ class AngelTypeController extends ApiController
             ->get(['id', 'name', 'description']);
 
         $models->map(function (AngelType $model): void {
-            $model->url = $this->url->to('/angeltypes', ['action' => 'view', 'angeltype_id' => $model->id]);
+            $model->url = $this->getUrl($model);
         });
 
         $data = ['data' => $models];
         return $this->response
             ->withContent(json_encode($data));
+    }
+
+    public function ofUser(Request $request): Response
+    {
+        $id = (int) $request->getAttribute('user_id');
+        $model = User::findOrFail($id);
+
+        $models = $model->userAngelTypes()->get([
+            'angel_types.id',
+            'angel_types.name',
+            'angel_types.description',
+            'angel_types.restricted',
+        ]);
+        $data = [];
+
+        $models->map(function (AngelType $model) use (&$data): void {
+            $model->confirmed = !$model->restricted || $model->pivot->supporter || $model->pivot->confirm_user_id;
+            $model->supporter = $model->pivot->supporter;
+            $model->url = $this->getUrl($model);
+            $modelData = $model->attributesToArray();
+            unset($modelData['restricted']);
+            $data[] = $modelData;
+        });
+
+        $data = ['data' => $data];
+        return $this->response
+            ->withContent(json_encode($data));
+    }
+
+    protected function getUrl(AngelType $model): string
+    {
+        return $this->url->to('/angeltypes', ['action' => 'view', 'angeltype_id' => $model->id]);
     }
 }
--- tests/Unit/Controllers/Api/AngelTypeControllerTest.php
+++ tests/Unit/Controllers/Api/AngelTypeControllerTest.php
@@ -5,13 +5,17 @@ declare(strict_types=1);
 namespace Engelsystem\Test\Unit\Controllers\Api;
 
 use Engelsystem\Controllers\Api\AngelTypeController;
+use Engelsystem\Http\Request;
 use Engelsystem\Http\Response;
 use Engelsystem\Models\AngelType;
+use Engelsystem\Models\User\User;
+use Engelsystem\Models\UserAngelType;
 
 class AngelTypeControllerTest extends ApiBaseControllerTest
 {
     /**
      * @covers \Engelsystem\Controllers\Api\AngelTypeController::index
+     * @covers \Engelsystem\Controllers\Api\AngelTypeController::getUrl
      */
     public function testIndex(): void
     {
@@ -31,6 +35,30 @@ class AngelTypeControllerTest extends ApiBaseControllerTest
         $this->assertCount(3, $data['data']);
         $this->assertCount(1, collect($data['data'])->filter(function ($item) use ($items) {
             return $item['name'] == $items->first()->getAttribute('name');
+        }));
+    }
+    /**
+     * @covers \Engelsystem\Controllers\Api\AngelTypeController::ofUser
+     */
+    public function testOfUser(): void
+    {
+        $this->initDatabase();
+        $user = User::factory()->create();
+        $items = UserAngelType::factory(3)->create(['user_id' => $user->id]);
+
+        $controller = new AngelTypeController(new Response(), $this->url);
+
+        $response = $controller->ofUser(new Request([], [], ['user_id' => $user->id]));
+        $this->validateApiResponse('/users/{id}/angeltypes', 'get', $response);
+
+        $this->assertEquals(['application/json'], $response->getHeader('content-type'));
+        $this->assertJson($response->getContent());
+
+        $data = json_decode($response->getContent(), true);
+        $this->assertArrayHasKey('data', $data);
+        $this->assertCount(3, $data['data']);
+        $this->assertCount(1, collect($data['data'])->filter(function ($item) use ($items) {
+            return $item['name'] == $items->first()->angelType->name;
         }));
     }
 }
