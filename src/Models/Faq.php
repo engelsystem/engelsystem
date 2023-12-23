648e4cf718f7e482702a56663d242968109cf06a
Igor Scheller <igor.scheller@igorshp.de>
2023-12-23 19:46:37 +0100

    Faq: Added tags

--- src/Models/Faq.php
+++ src/Models/Faq.php
@@ -6,7 +6,9 @@ namespace Engelsystem\Models;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 /**
  * @property int         $id
@@ -14,6 +16,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
  * @property string      $text
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ *
+ * @property-read Collection|Tag[] $tags
  *
  * @method static Builder|Faq whereId($value)
  * @method static Builder|Faq whereQuestion($value)
@@ -34,4 +38,9 @@ class Faq extends BaseModel
         'question',
         'text',
     ];
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class, 'faq_tags');
+    }
 }
