dbbf30e233311067e7cb2513258e7f37bc8f753b
Igor Scheller <igor.scheller@igorshp.de>
2024-01-02 13:51:20 +0100

    Replace empty or too short api keys instead of resetting them

--- db/migrations/2023_05_21_000001_cleanup_short_api_keys.php
+++ db/migrations/2023_05_21_000001_cleanup_short_api_keys.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Engelsystem\Migrations;
 
 use Engelsystem\Database\Migration\Migration;
+use Illuminate\Support\Str;
 
 class CleanupShortApiKeys extends Migration
 {
@@ -14,8 +15,14 @@ class CleanupShortApiKeys extends Migration
     public function up(): void
     {
         $db = $this->schema->getConnection();
-        $db->table('users')
-            ->where($db->raw('LENGTH(api_key)'), '<=', 42)
-            ->update(['api_key' => '']);
+        foreach ($db->table('users')->get() as $user) {
+            if (Str::length($user->api_key) > 42) {
+                continue;
+            }
+
+            $db->table('users')
+                ->where('id', $user->id)
+                ->update(['api_key' => bin2hex(random_bytes(32))]);
+        }
     }
 }
