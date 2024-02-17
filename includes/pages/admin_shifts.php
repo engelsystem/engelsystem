5e505cb8d27b5d80923b40171184b308782bdd85
Lotte Steenbrink <lotte@zombietetris.de>
2024-02-17 16:32:18 +0100

    note that shift length is in minutes

--- includes/pages/admin_shifts.php
+++ includes/pages/admin_shifts.php
@@ -547,7 +547,7 @@ function admin_shifts()
                         form_radio('mode', __('Create multiple shifts'), $mode == 'multi', 'multi'),
                         form_text(
                             'length',
-                            __('Length'),
+                            __('Length (in minutes)'),
                             $request->has('length')
                                 ? $request->input('length')
                                 : '120',
