2b0d7a8b8b3eafb0d8d34700314799acfd2326a3
Rusty Braze <180222500+RustyBraze@users.noreply.github.com>
2024-09-24 19:41:36 +0200

    Bugfix and Docker adjustment (#61)
    [Changes]
    - Close bugfix #59 - function now returns 0 in case of error
    - Dockerfile
      - Include 2 new config files + browser update command
      - Fix .dockerignore
    
    [Add]
    - New .gitattributes files > make sure things are right during commit

--- includes/helper/legacy_helper.php
+++ includes/helper/legacy_helper.php
@@ -6,12 +6,29 @@ use Engelsystem\Renderer\Twig\Extensions\Globals;
 use Engelsystem\Helpers\Carbon;
 use Engelsystem\Helpers\DayOfEvent;
 
+/**
+ * Retrieves the current theme ID from the global settings.
+ *
+ * @return int Returns the theme ID if it exists and is an integer, otherwise returns 0.
+ */
 function theme_id(): int
 {
-    /** @var Globals $globals */
-    $globals = app(Globals::class);
-    $globals = $globals->getGlobals();
-    return $globals['themeId'];
+    try {
+        /** @var Globals $globals */
+        $globals = app(Globals::class);
+        $globals = $globals->getGlobals();
+
+        // Ensure 'themeId' key exists in the $globals array and it is an integer
+        if (isset($globals['themeId']) && is_int($globals['themeId'])) {
+            return $globals['themeId'];
+        }
+
+        // Return 0 if 'themeId' key is not set or not an integer
+        return 0;
+    } catch (\Exception $e) {
+        // Handle any exceptions and return 0
+        return 0;
+    }
 }
 
 function theme(): array
