6edc140b5458883de4cf0a44a6b8f19527a11fe9
Igor Scheller <igor.scheller@igorshp.de>
2024-11-26 23:19:42 +0100

    Upgrade validation

--- src/Http/Validation/Rules/Max.php
+++ src/Http/Validation/Rules/Max.php
@@ -4,9 +4,15 @@ declare(strict_types=1);
 
 namespace Engelsystem\Http\Validation\Rules;
 
+use Respect\Validation\Rules\AbstractEnvelope;
 use Respect\Validation\Rules\Max as RespectMax;
 
-class Max extends RespectMax
+class Max extends AbstractEnvelope
 {
     use StringInputLength;
+
+    public function __construct(mixed $maxValue)
+    {
+        parent::__construct(new RespectMax($maxValue));
+    }
 }
