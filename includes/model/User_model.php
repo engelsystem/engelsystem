c7c3cd31477394574e9a2bc7a4871697c28cf451
Igor Scheller <igor.scheller@igorshp.de>
2024-12-04 21:10:34 +0100

    Move night shift calculation to goodie helper

--- includes/model/User_model.php
+++ includes/model/User_model.php
@@ -1,45 +1,11 @@
 <?php
 
 use Carbon\Carbon;
-use Engelsystem\Database\Db;
 use Engelsystem\Models\User\User;
-use Engelsystem\Models\Worklog;
 
 /**
  * User model
  */
-
-/**
- * Returns the goodie score (number of hours counted for tshirt).
- * Accounts only ended shifts.
- *
- * @param int $userId
- * @return float
- */
-function User_goodie_score(int $userId): float
-{
-    $shift_sum_formula = User_get_shifts_sum_query();
-    $result_shifts = Db::selectOne(sprintf('
-        SELECT ROUND((%s) / 3600, 2) AS `goodie_score`
-        FROM `users` LEFT JOIN `shift_entries` ON `users`.`id` = `shift_entries`.`user_id`
-        LEFT JOIN `shifts` ON `shift_entries`.`shift_id` = `shifts`.`id`
-        WHERE `users`.`id` = ?
-        AND `shifts`.`end` < NOW()
-        GROUP BY `users`.`id`
-    ', $shift_sum_formula), [
-        $userId,
-    ]);
-    if (!isset($result_shifts['goodie_score'])) {
-        $result_shifts = ['goodie_score' => 0];
-    }
-
-    $worklogHours = Worklog::query()
-        ->where('user_id', $userId)
-        ->where('worked_at', '<=', Carbon::Now())
-        ->sum('hours');
-
-    return $result_shifts['goodie_score'] + $worklogHours;
-}
 
 /**
  * @param User $user
@@ -84,45 +50,4 @@ function User_get_eligable_voucher_count($user)
     }
 
     return $vouchers;
-}
-
-/**
- * Generates the query to sum night shifts
- *
- * @return string
- */
-function User_get_shifts_sum_query()
-{
-    $nightShifts = config('night_shifts');
-    if (!$nightShifts['enabled']) {
-        return 'COALESCE(SUM(UNIX_TIMESTAMP(shifts.end) - UNIX_TIMESTAMP(shifts.start)), 0)';
-    }
-
-    /* @see \Engelsystem\Models\Shifts\Shift::isNightShift to keep it in sync */
-    return sprintf(
-        '
-            COALESCE(SUM(
-                (1 + (
-                    /* Starts during night */
-                    HOUR(shifts.start) >= %1$d AND HOUR(shifts.start) < %2$d
-                    /* Ends during night */
-                    OR (
-                        HOUR(shifts.end) > %1$d
-                        || HOUR(shifts.end) = %1$d AND MINUTE(shifts.end) > 0
-                    ) AND HOUR(shifts.end) <= %2$d
-                    /* Starts before and ends after night */
-                    OR HOUR(shifts.start) <= %1$d AND HOUR(shifts.end) >= %2$d
-                ))
-                * (UNIX_TIMESTAMP(shifts.end) - UNIX_TIMESTAMP(shifts.start))
-                * (1 - (%3$d + 1)
-                * (CASE
-                    WHEN `shift_entries`.`freeloaded_by` IS NULL THEN 0
-                    ELSE 1
-                    END))
-            ), 0)
-        ',
-        $nightShifts['start'],
-        $nightShifts['end'],
-        $nightShifts['multiplier']
-    );
 }
---
+++ src/Helpers/Goodie.php
@@ -0,0 +1,120 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Engelsystem\Helpers;
+
+use Engelsystem\Database\Database;
+use Engelsystem\Models\User\User;
+use Illuminate\Database\Query\Expression;
+use Illuminate\Database\Query\Grammars\MySqlGrammar;
+
+class Goodie
+{
+    /**
+     * Generates the query to sum night shifts
+     *
+     * Shifts and shift entries must be available via join
+     */
+    public static function shiftScoreQuery(): Expression
+    {
+        $nightShifts = config('night_shifts');
+
+        /** @var Database $db */
+        $db = app(Database::class);
+        $connection = $db->getConnection();
+
+        if (!$connection->getQueryGrammar() instanceof MySqlGrammar) {
+            return $connection->raw('0');
+        }
+
+        // @codeCoverageIgnoreStart
+        // as sqlite does not support TIMESTAMPDIFF
+
+        if (!$nightShifts['enabled']) {
+            return $connection->raw(
+                /** @lang MySQL */
+                'COALESCE(SUM(TIMESTAMPDIFF(MINUTE, shifts.start, shifts.end) * 60), 0)'
+            );
+        }
+
+        /* @see \Engelsystem\Models\Shifts\Shift::isNightShift to keep them in sync */
+        $query =
+            /** @lang MySQL */
+            '
+                COALESCE(SUM(
+                    /* Shift length */
+                    TIMESTAMPDIFF(MINUTE, shifts.start, shifts.end) * 60
+
+                    /* Handle freeloading */
+                    * (
+                        1
+                        - (%3$d + 1)
+                        * (
+                            CASE WHEN `shift_entries`.`freeloaded_by` IS NULL
+                            THEN 0
+                            ELSE 1
+                            END
+                        )
+                    )
+
+                    /** Is night shift */
+                    * (1 + (
+                        /* Starts during night */
+                        HOUR(shifts.start) >= %1$d AND HOUR(shifts.start) < %2$d
+                        /* Ends during night */
+                        OR (
+                            HOUR(shifts.end) > %1$d
+                            || HOUR(shifts.end) = %1$d AND MINUTE(shifts.end) > 0
+                        ) AND HOUR(shifts.end) <= %2$d
+                        /* Starts before and ends after night */
+                        OR HOUR(shifts.start) <= %1$d AND HOUR(shifts.end) >= %2$d
+                    ))
+                ), 0)
+            ';
+
+        $query = sprintf($query, $nightShifts['start'], $nightShifts['end'], $nightShifts['multiplier']);
+
+        return $connection->raw($query);
+        // @codeCoverageIgnoreEnd
+    }
+
+    /**
+     * Returns the goodie score (number of hours counted for goodie score)
+     * Includes only ended shifts
+     */
+    public static function userScore(User $user): float
+    {
+        /** @var Database $db */
+        $db = app(Database::class);
+        $con = $db->getConnection();
+
+        $state = $con
+            ->query()
+            ->from('users')
+            ->selectRaw(sprintf(
+                /** @lang MySQL */
+                'ROUND((%s) / 3600, 2) AS `goodie_score`',
+                self::shiftScoreQuery()->getValue($con->getQueryGrammar())
+            ))
+            ->where('users.id', $user->id)
+            ->join('shift_entries', 'users.id', 'shift_entries.user_id')
+            ->join('shifts', 'shift_entries.shift_id', 'shifts.id')
+            ->where('shifts.end', '<', Carbon::now())
+            ->groupBy('users.id')
+            ->first();
+
+        $shiftHours = 0;
+        if ($state) {
+            // @codeCoverageIgnoreStart
+            $shiftHours = (float) $state->goodie_score;
+            // @codeCoverageIgnoreEnd
+        }
+
+        $worklogHours = $user->worklogs()
+            ->where('worked_at', '<=', Carbon::Now())
+            ->sum('hours');
+
+        return $shiftHours + $worklogHours;
+    }
+}
