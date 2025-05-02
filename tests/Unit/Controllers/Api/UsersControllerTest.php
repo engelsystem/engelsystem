88fa3fe37a1718a9bae6d846967272a026c855c7
Igor Scheller <igor.scheller@igorshp.de>
2025-05-03 00:30:34 +0200

    Api: Added /users/[id]/worklogs

--- tests/Unit/Controllers/Api/UsersControllerTest.php
+++ tests/Unit/Controllers/Api/UsersControllerTest.php
@@ -14,6 +14,7 @@ use Engelsystem\Models\User\PersonalData;
 use Engelsystem\Models\User\Settings;
 use Engelsystem\Models\User\State;
 use Engelsystem\Models\User\User;
+use Engelsystem\Models\Worklog;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class UsersControllerTest extends ApiBaseControllerTest
@@ -153,5 +154,38 @@ class UsersControllerTest extends ApiBaseControllerTest
         $firstUser = $firstEntry['user'];
         $this->assertArrayHasKey('id', $firstUser);
         $this->assertEquals($user->id, $firstUser['id']);
+    }
+
+    /**
+     * @covers \Engelsystem\Controllers\Api\UsersController::worklogs
+     * @covers \Engelsystem\Controllers\Api\Resources\WorklogResource::toArray
+     */
+    public function testWorklogs(): void
+    {
+        /** @var User $user */
+        $user = User::factory()->create();
+        $controller = new UsersController(new Response());
+        /** @var Worklog $worklog */
+        $worklog = Worklog::factory()->create(['user_id' => $user->id, 'hours' => 1.23]);
+        $request = new Request([], [], ['user_id' => $user->id]);
+
+        $response = $controller->worklogs($request);
+        $this->validateApiResponse('/users/{id}/worklogs', 'get', $response);
+
+        $this->assertEquals(['application/json'], $response->getHeader('content-type'));
+        $this->assertJson($response->getContent());
+
+        $data = json_decode($response->getContent(), true);
+
+        $this->assertArrayHasKey('data', $data);
+        $this->assertIsArray($data['data']);
+        $this->assertNotEmpty($data['data']);
+
+        $firstEntry = $data['data'][0];
+        $this->assertArrayHasKey('id', $firstEntry);
+        $this->assertArrayHasKey('description', $firstEntry);
+        $this->assertArrayHasKey('hours', $firstEntry);
+
+        $this->assertEquals($worklog->hours, $firstEntry['hours']);
     }
 }
