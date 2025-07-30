86454ce0ce05641356e349d3a105c5e9392da959
Gereon Kremer <gereon.kremer@gmail.com>
2025-07-30 21:46:56 +0200

    Hide some obsolete columns (#18)
    I've always been bothered by columns in the user list that we never use. This removes them.

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    // Hide columns in backend user view. Possible values are any sortable parameters of the table.
+    'disabled_user_view_columns' => ['freeloads', 'active', 'arrival_date', 'departure_date'],
+];
