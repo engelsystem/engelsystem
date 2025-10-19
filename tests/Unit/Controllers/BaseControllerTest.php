4afa59e34d6664f7b8e48d44028b098253ecd851
Igor Scheller <igor.scheller@igorshp.de>
2025-10-19 12:56:39 +0200

    BaseController: Add hasPermission

--- tests/Unit/Controllers/BaseControllerTest.php
+++ tests/Unit/Controllers/BaseControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Engelsystem\Test\Unit\Controllers;
 
+use Engelsystem\Http\Request;
 use Engelsystem\Test\Unit\Controllers\Stub\ControllerImplementation;
 use PHPUnit\Framework\TestCase;
 
@@ -25,5 +26,18 @@ class BaseControllerTest extends TestCase
         ], $controller->getPermissions());
 
         $this->assertTrue(method_exists($controller, 'setValidator'));
+    }
+
+    /**
+     * @covers \Engelsystem\Controllers\BaseController::hasPermission
+     */
+    public function testHasPermission(): void
+    {
+        $request = new Request();
+        $controller = new ControllerImplementation();
+
+        $this->assertTrue($controller->hasPermission($request, 'yay'));
+        $this->assertNull($controller->hasPermission($request, 'test'));
+        $this->assertFalse($controller->hasPermission($request, 'nope'));
     }
 }
