af4f7afdcc10a4380cf4c213fc980f4b36856ec1
Xu <git@xumail.de>
2025-12-16 14:22:38 +0100

    linter

--- db/migrations/2025_12_13_000000_add_goodie_score_calculation_function.php
+++ db/migrations/2025_12_13_000000_add_goodie_score_calculation_function.php
@@ -26,38 +26,38 @@ class AddGoodieScoreCalculationFunction extends Migration
         $this->db->statement(
         /** @lang MySQL */
             'CREATE DEFINER=`engelsystem`@`%` FUNCTION `goodie_score` (
-                    `user_id` INT, 
-                    `night_shift_start` INT, 
-                    `night_shift_end` INT, 
+                    `user_id` INT,
+                    `night_shift_start` INT,
+                    `night_shift_end` INT,
                     `night_shift_multiplier` INT,
                     `now` TIMESTAMP
-                ) RETURNS INT(11)  
+                ) RETURNS INT(11)
                 BEGIN
                     DECLARE done INT;
                     DECLARE shift_start TIMESTAMP;
                     DECLARE shift_end TIMESTAMP;
                     DECLARE freeloaded BOOLEAN;
-                
+
                     DECLARE START_OF_DAY TIMESTAMP;
                     DECLARE NIGHT_START TIMESTAMP;
                     DECLARE NIGHT_END TIMESTAMP;
                     DECLARE NIGHT_SHIFT_SECONDS int;
                     DECLARE SHIFT_SUM int;
                     DECLARE GOODY_SUM int;
-                    
+
                     DECLARE DAYS_DIFF int;
-                
+
                     DECLARE shift_cursor CURSOR FOR
-                            SELECT s.start, s.end, se.freeloaded_by IS NOT NULL AS freeloaded 
+                            SELECT s.start, s.end, se.freeloaded_by IS NOT NULL AS freeloaded
                             FROM users u
                             JOIN shift_entries se ON se.user_id = u.id
                             JOIN shifts s ON se.shift_id = s.id
                             WHERE u.id = user_id
                             AND s.end < now;
                     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-                
+
                     OPEN shift_cursor;
-                    
+
                     Set GOODY_SUM = 0;
                     read_loop: LOOP
                         FETCH shift_cursor INTO shift_start, shift_end, freeloaded;
@@ -70,23 +70,23 @@ class AddGoodieScoreCalculationFunction extends Migration
                             SET DAYS_DIFF = 0;
                             SET NIGHT_SHIFT_SECONDS = 0;
                             SET START_OF_DAY = TIMESTAMP(DATE(shift_start));
-                            
+
                             WHILE START_OF_DAY < shift_end DO
                                 SET NIGHT_START = TIMESTAMPADD(HOUR, night_shift_start, START_OF_DAY);
                                 SET NIGHT_END = TIMESTAMPADD(HOUR, night_shift_end, START_OF_DAY);
                                 SET NIGHT_SHIFT_SECONDS = NIGHT_SHIFT_SECONDS + GREATEST(0, TIMESTAMPDIFF(
-                                    SECOND, 
+                                    SECOND,
                                     GREATEST(shift_start, NIGHT_START),
                                     LEAST(shift_end, NIGHT_END)
                                 ));
                                 SET DAYS_DIFF = DAYS_DIFF + 1;
                                 SET START_OF_DAY = TIMESTAMPADD(DAY, 1 , START_OF_DAY);
                             END WHILE;
-                            
-                            SET SHIFT_SUM = TIMESTAMPDIFF(SECOND, shift_start, shift_end) 
+
+                            SET SHIFT_SUM = TIMESTAMPDIFF(SECOND, shift_start, shift_end)
                                                 + (NIGHT_SHIFT_SECONDS * (night_shift_multiplier - 1));
                         END IF;
-                    
+
                         SET GOODY_SUM = GOODY_SUM + SHIFT_SUM;
                     END LOOP;
                     CLOSE shift_cursor;
