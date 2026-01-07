2ab885ed98717b4d402ccf33d71707da59f526f7
Gabriela Bogk <gabriela.bogk@adevinta.com>
2026-01-07 18:09:35 +0100

    fix: Remove MYSQL_PORT from nix branch (belongs in minor-volunteers)

--- tests/Feature/Database/DatabaseTest.php
+++ tests/Feature/Database/DatabaseTest.php
@@ -17,7 +17,6 @@ abstract class DatabaseTest extends TestCase
     {
         $configValues = ['database' => [
             'host' => env('MYSQL_HOST', 'localhost'),
-            'port' => (int) env('MYSQL_PORT', 3306),
             'database' => env('MYSQL_DATABASE', 'engelsystem'),
             'username' => env('MYSQL_USER', 'root'),
             'password' => env('MYSQL_PASSWORD', ''),
