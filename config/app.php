a264fb8d15e16459ecfe596fbc0fa2256f91d958
Christian Eichler <code@christian-eichler.de>
2026-01-25 09:12:27 +0100

    feat: Allow hiding empty time slots in calendar view

--- config/app.php
+++ config/app.php
@@ -272,6 +272,10 @@ return [
                     'type' => 'boolean',
                     'default' => true,
                 ],
+                'shift_view.enable_compact_view' => [
+                    'type' => 'boolean',
+                    'default' => false,
+                ],
             ],
         ],
 
