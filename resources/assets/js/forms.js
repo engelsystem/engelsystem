35d19376d34946dea5b1f253c46a5b64b87198e0
Igor Scheller <igor.scheller@igorshp.de>
2026-03-06 22:20:47 +0100

    iCal button: Show popover with link

--- resources/assets/js/forms.js
+++ resources/assets/js/forms.js
@@ -271,6 +271,14 @@ ready(() => {
 });
 
 /**
+ * Remove links from popover buttons
+ */
+ready(() => {
+  // biome-ignore lint/suspicious/useIterableCallbackReturn: ...
+  document.querySelectorAll('[data-js-link-remove="true"]').forEach((element) => (element.href = '#'));
+});
+
+/**
  * Init Bootstrap Tooltips
  */
 ready(() => {
