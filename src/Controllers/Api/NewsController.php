9e98b3667656aa0cddc44be26f04dcbec1128898
cookie <cookie@cuccato.net>
2026-09-22 11:50:29 +0000

    API: Don't require "api" privilege for GET /news
    News are gated by the "news" privilege on the website, which regular
    users have - there's no reason the API variant should additionally
    require the "api" privilege. Any authenticated user (valid API
    key/bearer token) can now read /api/v0-beta/news, matching the intent
    of the "self" endpoints.

--- src/Controllers/Api/NewsController.php
+++ src/Controllers/Api/NewsController.php
@@ -7,9 +7,21 @@ namespace Engelsystem\Controllers\Api;
 use Engelsystem\Controllers\Api\Resources\NewsResource;
 use Engelsystem\Http\Response;
 use Engelsystem\Models\News;
+use Psr\Http\Message\ServerRequestInterface;
 
 class NewsController extends ApiController
 {
+    use UsesAuth;
+
+    /**
+     * News don't require the "api" privilege, unlike the rest of the API - any
+     * authenticated user may read them, same as on the website (the "news" privilege).
+     */
+    public function hasPermission(ServerRequestInterface $request, string $method): ?bool
+    {
+        return (bool) $this->auth?->user();
+    }
+
     public function index(): Response
     {
         $models = News::query()
